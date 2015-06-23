76886e39f98b12b754a8ac33f6ae4d933d0b93de
Evgenii Pashkin <eapashkin@gmail.com>
2015-06-23 10:31:02 +0300

    Create backup file, if none

--- src/file_saver.rs
+++ src/file_saver.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::fs;
 use std::fs::File;
 use std::io::{Result, Write};
 use std::path::Path;
@@ -11,7 +12,11 @@ pub trait SaveToFile {
 
 impl<I: Iterator<Item=Chunk>> SaveToFile for I {
     fn save_to_file<P: AsRef<Path>>(&mut self, path: P) -> Result<()> {
-        //TODO: add backup
+        match create_backup(&path) {
+            Err(why) => panic!("couldn't create backup for {:?}: {}", path.as_ref(),
+                            Error::description(&why)),
+            Ok(_) => (),
+        }
         let mut file = match File::create(&path) {
             Err(why) => panic!("couldn't create {:?}: {}", path.as_ref(),
                             Error::description(&why)),
@@ -27,4 +32,18 @@ impl<I: Iterator<Item=Chunk>> SaveToFile for I {
 
         Ok(())
     }
+}
+
+/// Create .bak file, only if one not present
+pub fn create_backup<P: AsRef<Path>>(path: P) -> Result<bool> {
+    match fs::metadata(&path) {
+        Err(_) => return Ok(false),
+        Ok(_) => (),
+    }
+    let new_path = path.as_ref().with_extension("bak");
+    match fs::metadata(&new_path) {
+        Err(_) => (),
+        Ok(_) => return Ok(false),
+    }
+    fs::rename(path, new_path).map(|_| true)
 }
