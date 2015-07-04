2886ce4be74f273686763eeaf99af17a72c81fa8
Evgenii Pashkin <eapashkin@gmail.com>
2015-07-04 06:00:24 +0300

    Separate type kinds for library::Fundamental

--- src/analysis/type_kind.rs
+++ src/analysis/type_kind.rs
@@ -20,10 +20,45 @@ pub trait ToTypeKind {
     fn to_type_kind(&self, library: &Library) -> TypeKind;
 }
 
+impl ToTypeKind for Fundamental {
+    fn to_type_kind(&self, _library: &Library) -> TypeKind {
+        use library::Fundamental::*;
+        match self {
+            &Boolean => TypeKind::Simple,
+            &Int8 => TypeKind::Simple,
+            &UInt8 => TypeKind::Simple,
+            &Int16 => TypeKind::Simple,
+            &UInt16 => TypeKind::Simple,
+            &Int32 => TypeKind::Simple,
+            &UInt32 => TypeKind::Simple,
+            &Int64 => TypeKind::Simple,
+            &UInt64 => TypeKind::Simple,
+            &Char => TypeKind::Simple,
+            &UChar => TypeKind::Simple,
+            &Int => TypeKind::Simple,
+            &UInt => TypeKind::Simple,
+            &Long => TypeKind::Simple,
+            &ULong => TypeKind::Simple,
+            &Size => TypeKind::Simple,
+            &SSize => TypeKind::Simple,
+            &Float => TypeKind::Simple,
+            &Double => TypeKind::Simple,
+            &UniChar => TypeKind::Unknown,
+            &Pointer => TypeKind::Pointer,
+            &VarArgs => TypeKind::Unknown,
+            &Utf8 => TypeKind::Pointer,
+            &Filename => TypeKind::Pointer,
+            &Type => TypeKind::Enumeration,
+            &None => TypeKind::Unknown,
+            &Unsupported => TypeKind::Unknown,
+        }
+    }
+}
+
 impl ToTypeKind for Type {
     fn to_type_kind(&self, library: &Library) -> TypeKind {
         match self {
-            &Type::Fundamental(_) => TypeKind::Simple,
+            &Type::Fundamental(fund) => fund.to_type_kind(library),
             &Type::Enumeration(_) => TypeKind::Enumeration,
             &Type::Interface(_) => TypeKind::Interface,
             &Type::Class(ref klass) => {
