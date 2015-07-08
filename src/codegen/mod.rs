40211746a159bd05f98952a5321b495d1bce7438
Evgenii Pashkin <eapashkin@gmail.com>
2015-07-08 07:20:19 +0300

    Add work modes: normal (widgets) and sys (currently empty)

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -1,15 +1,20 @@
 use env::Env;
+use config::WorkMode;
 
 mod function;
 mod function_body;
 mod general;
 mod parameter;
 mod return_value;
+mod sys;
 mod translate_from_glib;
 mod translate_to_glib;
 mod widget;
 mod widgets;
 
 pub fn generate(env: &Env) {
-    widgets::generate(env);
+    match env.config.work_mode {
+        WorkMode::Normal => widgets::generate(env),
+        WorkMode::Sys => sys::generate(env),
+    }
 }
---
+++ src/codegen/sys/mod.rs
@@ -0,0 +1,5 @@
+use env::Env;
+
+pub fn generate(env: &Env) {
+    println!("generating sys for {}", env.config.library_name);
+}
