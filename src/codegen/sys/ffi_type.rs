9bce3bd7516236d0566581ac8ab4f591c17ed4b4
Evgenii Pashkin <eapashkin@gmail.com>
2015-07-09 08:47:44 +0300

    Add prefix for external types

--- src/codegen/sys/ffi_type.rs
+++ src/codegen/sys/ffi_type.rs
@@ -1,12 +1,14 @@
-use env::Env;
-use library;
 use analysis::rust_type::Result;
+use env::Env;
+use gobjects::GStatus;
+use library;
+use nameutil::module_name;
 
 pub fn ffi_type(env: &Env, type_id: library::TypeId) -> Result {
     use library::Type::*;
     use library::Fundamental::*;
     let type_ = env.library.type_(type_id);
-    let ffi_type = match type_ {
+    match type_ {
         &Fundamental(fund) => {
             let ok = |s: &str| Ok(s.into());
             let err = |s: &str| Err(s.into());
@@ -37,10 +39,30 @@ pub fn ffi_type(env: &Env, type_id: library::TypeId) -> Result {
         },
 
         &Enumeration(ref enum_) => Ok(enum_.glib_type_name.clone()),
-        &Interface(ref interface) => Ok(format!("*mut {}", interface.glib_type_name)),
-        &Class(ref klass) => Ok(format!("*mut {}", klass.glib_type_name)),
+        &Interface(ref interface) => to_mut_ptr(fix_external_name(env, type_id, &interface.glib_type_name)),
+        &Class(ref klass) => to_mut_ptr(fix_external_name(env, type_id, &klass.glib_type_name)),
         _ => Err(format!("Unknown rust type: {:?}", type_.get_name() )),
         //TODO: check usage library::Type::get_name() when no _ in this
-    };
-    ffi_type
+    }
+}
+
+fn fix_external_name(env: &Env, type_id: library::TypeId, name: &str) -> Result {
+    if type_id.ns_id == library::MAIN_NAMESPACE || type_id.ns_id == library::INTERNAL_NAMESPACE {
+        Ok(name.into())
+    } else {
+        let name_with_prefix = format!("{}_ffi::{}",
+            module_name(&env.library.namespace(type_id.ns_id).name), name);
+        if env.type_status(&type_id.full_name(&env.library)) == GStatus::Ignore {
+            Err(name_with_prefix.into())
+        } else {
+            Ok(name_with_prefix)
+        }
+    }
+}
+
+fn to_mut_ptr(res: Result) -> Result {
+    match res {
+        Ok(s) => Ok(format!("*mut {}", s)),
+        Err(s) => Err(format!("*mut {}", s)),
+    }
 }
