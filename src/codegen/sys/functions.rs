0032400979e7672cf043afcaa3d8ea90413f3542
Evgenii Pashkin <eapashkin@gmail.com>
2015-07-09 07:27:38 +0300

    Generate fii with static text and classes headers

--- src/codegen/sys/functions.rs
+++ src/codegen/sys/functions.rs
@@ -3,7 +3,9 @@ use std::io::{Result, Write};
 
 use env::Env;
 use file_saver::*;
+use library;
 use nameutil::*;
+use super::statics;
 use super::super::general;
 
 pub fn generate(env: &Env) {
@@ -18,6 +20,44 @@ pub fn generate(env: &Env) {
 
 fn generate_funcs<W: Write>(w: &mut W, env: &Env) -> Result<()>{
     try!(general::start_comments(w, &env.config));
+
+    try!(statics::generate(w));
+
+    let ns_id = library::MAIN_NAMESPACE;
+    let ns = env.library.namespace(ns_id);
+
+    try!(writeln!(w, ""));
+    try!(writeln!(w, "extern \"C\" {{"));
+    try!(generate_classes_funcs(w, env, ns_id, ns));
+
+    //TODO: other functions
+    try!(writeln!(w, "\n}}"));
+
+    Ok(())
+}
+fn generate_classes_funcs<W: Write>(w: &mut W, env: &Env, ns_id: u16, ns:&library::Namespace) -> Result<()> {
+    let mut vec: Vec<(library::TypeId, &library::Class)> = Vec::with_capacity(ns.types.len());
+    for id in 0..ns.types.len() {
+        let tid = library::TypeId { ns_id: ns_id, id: id as u32 };
+        if let &library::Type::Class(ref klass) = env.library.type_(tid) {
+            vec.push((tid, klass));
+        }
+    }
+    vec.sort_by(|&(_, klass1), &(_, klass2)| klass1.glib_type_name.cmp(&klass2.glib_type_name));
+
+    for (_, ref klass) in vec {
+        try!(generate_class_funcs(w, klass));
+    }
+
+    Ok(())
+}
+
+fn generate_class_funcs<W: Write>(w: &mut W, klass: &library::Class) -> Result<()> {
+    try!(writeln!(w, ""));
+    try!(writeln!(w, "    //========================================================================="));
+    try!(writeln!(w, "    // {}", klass.glib_type_name));
+    try!(writeln!(w, "    //========================================================================="));
+    try!(writeln!(w, "    pub fn {:<36}() -> GType;", klass.glib_get_type));
 
     Ok(())
 }
--- src/codegen/sys/mod.rs
+++ src/codegen/sys/mod.rs
@@ -1,6 +1,7 @@
 use env::Env;
 
 mod functions;
+mod statics;
 
 pub fn generate(env: &Env) {
     functions::generate(env);
