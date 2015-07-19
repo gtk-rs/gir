c8f3b613ca1e3adc74e1cea77a3c318b272ca9eb
Evgenii Pashkin <eapashkin@gmail.com>
2015-07-19 08:00:27 +0300

    Fix build errors

--- src/codegen/sys/statics.rs
+++ src/codegen/sys/statics.rs
@@ -5,7 +5,7 @@ use super::super::general::write_vec;
 pub fn begin<W: Write>(w: &mut W) -> Result<()>{
     let v = vec![
 "",
-"#![allow(non_camel_case_types)]",
+"#![allow(non_camel_case_types, non_upper_case_globals)]",
 "",
 "extern crate libc;",
 "#[macro_use] extern crate bitflags;",
@@ -58,14 +58,6 @@ pub fn only_for_gtk<W: Write>(w: &mut W) -> Result<()>{
     let v = vec![
 "",
 "pub const GTK_ENTRY_BUFFER_MAX_SIZE: u16 = ::std::u16::MAX;",
-"",
-"//pub type GtkTreeModelForeachFunc = fn(model: *mut GtkTreeModel, path: *mut GtkTreePath, iter: *mut GtkTreeIter, data: gpointer) -> gboolean;",
-"",
-"pub const GTK_STYLE_PROVIDER_PRIORITY_FALLBACK: u32 = 1;",
-"pub const GTK_STYLE_PROVIDER_PRIORITY_THEME: u32 = 200;",
-"pub const GTK_STYLE_PROVIDER_PRIORITY_SETTINGS: u32 = 400;",
-"pub const GTK_STYLE_PROVIDER_PRIORITY_APPLICATION: u32 = 600;",
-"pub const GTK_STYLE_PROVIDER_PRIORITY_USER: u32 = 800;",
     ];
 
     write_vec(w, &v)
