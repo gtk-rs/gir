6c9eb9f651a4fc0a4aaecbf4c86dfbe8d779c07b
Gleb Kozyrev <gleb@gkoz.com>
2015-07-25 18:49:32 +0300

    sys: add Cargo.toml generation and manipulation

--- src/codegen/sys/mod.rs
+++ src/codegen/sys/mod.rs
@@ -5,8 +5,10 @@ mod ffi_type;
 mod functions;
 mod lib_;
 mod statics;
+mod cargo_toml;
 
 pub fn generate(env: &Env) {
     lib_::generate(env);
     build::generate(env);
+    cargo_toml::generate(env);
 }
