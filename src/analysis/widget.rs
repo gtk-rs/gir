43898d5d126041b708e694eaabcb5210fd70d0c5
Evgenii Pashkin <eapashkin@gmail.com>
2015-07-28 14:49:03 +0300

    Add GtkWindow, GtkDialog generation. Remove TODO assert

--- src/analysis/widget.rs
+++ src/analysis/widget.rs
@@ -1,13 +1,9 @@
 use env::Env;
 use gobjects::GObject;
-use library;
 use super::object;
 
 pub fn new(env: &Env, obj: &GObject) -> object::Info {
     let info = object::new(env, obj);
-
-    let has_functions = info.functions.iter().find(|f| f.kind == library::FunctionKind::Function).is_some();
-    assert!(!has_functions, "Widget {} has functions, to do functions code generation", info.full_name);
 
     info
 }
