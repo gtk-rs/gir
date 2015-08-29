e3422cd733ff4d1b135209503c8642af25ed819a
Gleb Kozyrev <gleb@gkoz.com>
2015-08-29 17:02:06 +0300

    Add upcasts and Option support to translate_to_glib

--- src/codegen/translate_to_glib.rs
+++ src/codegen/translate_to_glib.rs
@@ -2,37 +2,40 @@ use analysis::type_kind::TypeKind;
 use library;
 
 pub trait TranslateToGlib {
-    fn translate_to_glib(&self, library: &library::Library, in_trait: bool) -> String;
+    fn translate_to_glib(&self, library: &library::Library, upcast: bool) -> String;
 }
 
 impl TranslateToGlib for library::Parameter {
-    fn translate_to_glib(&self, library: &library::Library, in_trait: bool) -> String {
-        if self.instance_parameter {
-            let upcast_str = if in_trait { ".upcast()" } else { "" };
-            format!("self{}.to_glib_none().0", upcast_str)
-        } else {
-            let kind = TypeKind::of(library, self.typ);
-            match kind {
-                TypeKind::Converted => format_parameter(&self.name, "to_glib()"),
-                TypeKind::Pointer => format_parameter(&self.name, "to_glib_none().0"),
-                TypeKind::Direct |
-                    TypeKind::Enumeration => self.name.clone(),
-                TypeKind::Object => to_glib_xxx(&self.name, self.transfer),
-                _ => format!("TODO:{}", self.name)
+    fn translate_to_glib(&self, library: &library::Library, upcast: bool) -> String {
+        let kind = TypeKind::of(library, self.typ);
+        let upcast_str = match (upcast, self.nullable) {
+            (true, true) => ".map(Upcast::upcast)",
+            (true, false) => ".upcast()",
+            _ => "",
+        };
+        match kind {
+            TypeKind::Converted => format!("{}{}", self.name, ".to_glib()"),
+            TypeKind::Direct |
+                TypeKind::Enumeration => self.name.clone(),
+            TypeKind::Pointer |
+                TypeKind::Object => {
+                if self.instance_parameter {
+                    format!("self{}{}", upcast_str, to_glib_xxx(self.transfer))
+                }
+                else {
+                    format!("{}{}{}", self.name, upcast_str, to_glib_xxx(self.transfer))
+                }
             }
+            _ => format!("TODO:{}", self.name)
         }
     }
 }
 
-fn format_parameter(name: &str, convert: &str) -> String {
-    format!("{}.{}", name, convert)
-}
-
-fn to_glib_xxx(name: &str, transfer: library::Transfer) -> String {
+fn to_glib_xxx(transfer: library::Transfer) -> &'static str {
     use library::Transfer::*;
     match transfer {
-        None => format_parameter(name, "to_glib_none().0"),
-        Full => format_parameter(name, "to_glib_full().0"),
-        Container => format!("TODO:container {}", name),
+        None => ".to_glib_none().0",
+        Full => ".to_glib_full()",
+        Container => "TODO:container",
     }
 }
