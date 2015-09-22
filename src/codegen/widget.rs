8bfe67323c9b5f7d465db2474a42e916c0bfa1a1
Gleb Kozyrev <gleb@gkoz.com>
2015-09-22 21:38:45 +0300

    Put the autogenerated files in `src/auto` and add `src/auto/mod.rs`
    Reexport all types in the `auto` module.

--- src/codegen/widget.rs
+++ src/codegen/widget.rs
@@ -3,17 +3,10 @@ use std::io::{Result, Write};
 use analysis;
 use env::Env;
 use super::{function, general};
+use super::general::tabs;
 
 pub fn generate<W: Write>(w: &mut W, env: &Env, analysis: &analysis::object::Info) -> Result<()>{
     let type_ = analysis.type_(&env.library);
-
-    let mut generate_impl = false;
-    let mut generate_trait = false;
-
-    generate_impl |= analysis.has_constructors;
-    generate_impl |= analysis.has_functions;
-
-    if analysis.has_children { generate_trait |= true } else { generate_impl |= true };
 
     try!(general::start_comments(w, &env.config));
     try!(general::uses(w, &analysis.used_types));
@@ -21,14 +14,14 @@ pub fn generate<W: Write>(w: &mut W, env: &Env, analysis: &analysis::object::Inf
     try!(general::impl_parents(w, &analysis.name, &analysis.parents));
     try!(general::impl_interfaces(w, &analysis.name, &analysis.implements));
 
-    if generate_impl {
+    if generate_inherent(analysis) {
         try!(writeln!(w, ""));
         try!(writeln!(w, "impl {} {{", analysis.name));
         for func_analysis in &analysis.constructors() {
             try!(function::generate(w, env, func_analysis, false, false, 1));
         }
 
-        if !analysis.has_children {
+        if !generate_trait(analysis) {
             for func_analysis in &analysis.methods() {
                 try!(function::generate(w, env, func_analysis, false, false, 1));
             }
@@ -41,7 +34,7 @@ pub fn generate<W: Write>(w: &mut W, env: &Env, analysis: &analysis::object::Inf
     }
     try!(general::impl_static_type(w, &analysis.name, &type_.glib_get_type));
 
-    if generate_trait {
+    if generate_trait(analysis) {
         try!(writeln!(w, ""));
         try!(writeln!(w, "pub trait {}Ext {{", analysis.name));
         for func_analysis in &analysis.methods() {
@@ -58,4 +51,23 @@ pub fn generate<W: Write>(w: &mut W, env: &Env, analysis: &analysis::object::Inf
     }
 
     Ok(())
+}
+
+fn generate_inherent(analysis: &analysis::object::Info) -> bool {
+    analysis.has_constructors || analysis.has_functions || !analysis.has_children
+}
+
+fn generate_trait(analysis: &analysis::object::Info) -> bool {
+    analysis.has_children
+}
+
+pub fn generate_reexports(analysis: &analysis::object::Info, module_name: &str,
+        contents: &mut Vec<String>, traits: &mut Vec<String>) {
+    contents.push(format!(""));
+    contents.push(format!("mod {};", module_name));
+    contents.push(format!("pub use self::{}::{};", module_name, analysis.name));
+    if generate_trait(analysis) {
+        contents.push(format!("pub use self::{}::{}Ext;", module_name, analysis.name));
+        traits.push(format!("{}pub use super::{}Ext;", tabs(1), analysis.name));
+    }
 }
--- src/codegen/widgets.rs
+++ src/codegen/widgets.rs
@@ -1,13 +1,18 @@
-use std::path::PathBuf;
+use std::io::Write;
+use std::path::{Path, PathBuf};
 
 use analysis;
 use analysis::general::is_widget;
 use env::Env;
 use file_saver::*;
 use nameutil::*;
+use super::general;
 
 pub fn generate(env: &Env) {
-    let root_path = PathBuf::from(&env.config.target_path).join("src").join("widgets");
+    let root_path = PathBuf::from(&env.config.target_path).join("src").join("auto");
+
+    let mut mod_rs: Vec<String> = Vec::new();
+    let mut traits: Vec<String> = Vec::new();
 
     for obj in env.config.objects.values() {
         if !obj.status.need_generate() || !is_widget(&obj.name, &env.library){
@@ -26,5 +31,22 @@ pub fn generate(env: &Env) {
 
         save_to_file(path, env.config.make_backup,
             &mut |w| super::widget::generate(w, env, &class_analysis));
+
+        let mod_name = module_name(split_namespace_name(&class_analysis.full_name).1);
+        super::widget::generate_reexports(&class_analysis, &mod_name, &mut mod_rs, &mut traits);
     }
+
+    generate_mod_rs(env, &root_path, mod_rs, traits);
+}
+
+fn generate_mod_rs(env: &Env, root_path: &Path, mod_rs: Vec<String>, traits: Vec<String>) {
+    let path = root_path.join("mod.rs");
+    save_to_file(path, env.config.make_backup, &mut |w| {
+        try!(general::start_comments(w, &env.config));
+        try!(general::write_vec(w, &mod_rs));
+        try!(writeln!(w, ""));
+        try!(writeln!(w, "pub mod traits {{"));
+        try!(general::write_vec(w, &traits));
+        writeln!(w, "}}")
+    });
 }
