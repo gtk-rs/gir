1093987a863f26beca0edb36fda73565f7dcf4a9
Evgenii Pashkin <eapashkin@gmail.com>
2015-09-24 21:16:33 +0300

    Comment functions with unsupported outs

--- src/analysis/out_parameters.rs
+++ src/analysis/out_parameters.rs
@@ -6,22 +6,26 @@ use super::type_kind::TypeKind;
 
 pub type Info = Vec<Parameter>;
 
-pub fn analyze(env: &Env, type_: &Function) -> Info {
+pub fn analyze(env: &Env, type_: &Function) -> (Info, bool) {
     let mut outs = Info::new();
+    let mut unsupported_outs = false;
     //Only process out parameters if function returns None
-    if type_.ret.typ != Default::default() { return outs; }
+    if type_.ret.typ != Default::default() { return (outs, false); }
 
     for par in &type_.parameters {
+        if par.direction != ParameterDirection::Out { continue; }
         if can_as_return(env, par) {
             outs.push(par.clone());
+        } else {
+            unsupported_outs = true;
         }
     }
-    outs
+
+    (outs, unsupported_outs)
 }
 
 fn can_as_return(env: &Env, par: &Parameter) -> bool {
     use super::type_kind::TypeKind::*;
-    if par.direction != ParameterDirection::Out { return false; }
     match TypeKind::of(&env.library, par.typ) {
         Enumeration |
             Direct => true,
