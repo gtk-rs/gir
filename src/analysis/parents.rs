9f293c43555a942d8d6f0b99cbedf2b774118595
Gleb Kozyrev <gleb@gkoz.com>
2015-10-13 17:31:23 +0300

    Remove ignored_parents analysis

--- src/analysis/parents.rs
+++ src/analysis/parents.rs
@@ -8,15 +8,11 @@ use library::Class;
 use traits::*;
 
 pub fn analyze(env: &Env, type_: &Class, imports: &mut Imports)
-    -> (Vec<StatusedTypeId>, bool) {
+        -> Vec<StatusedTypeId> {
     let mut parents = Vec::new();
-    let mut has_ignored_parents = false;
 
     for &parent_tid in &type_.parents {
         let parent_type = env.type_(parent_tid).to_ref_as::<Class>();
-
-        if parent_type.c_type == "GObject" { break }
-
         let status = env.type_status(&parent_tid.full_name(&env.library));
 
         parents.push(StatusedTypeId{
@@ -24,13 +20,12 @@ pub fn analyze(env: &Env, type_: &Class, imports: &mut Imports)
             name: parent_type.name.clone(),
             status: status,
         });
-        used_rust_type(env, parent_tid).ok().map(|s| imports.add(s, None));
 
-        if status.ignored() { has_ignored_parents = true; }
-
-        if parent_type.c_type == "GtkWidget" { break }
+        if !status.ignored() {
+            used_rust_type(env, parent_tid).ok().map(|s| imports.add(s, None));
+        }
     }
-    parents.reverse();
 
-    (parents, has_ignored_parents)
+    parents.reverse();
+    parents
 }
