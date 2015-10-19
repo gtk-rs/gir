1c2ce6627bc5bee826fe8389cf07946d0364fd09
Evgenii Pashkin <eapashkin@gmail.com>
2015-10-19 22:41:50 +0300

    Add block formaters

--- src/writer/mod.rs
+++ src/writer/mod.rs
@@ -2,13 +2,12 @@ mod defines;
 pub mod primitives;
 
 pub use self::defines::{TAB, TAB_SIZE, MAX_TEXT_WIDTH};
-use self::primitives::tabs;
+use self::primitives::{comment_block, format_block, format_block_smart};
 
 //TODO: move to chunks
 pub fn ffi_function_todo(name: &str) -> Vec<String> {
-    let mut v = Vec::new();
-    v.push(format!("//{}unsafe {{ TODO: call ffi:{}() }}",
-                        tabs(1), name));
-    v.push(format!("//}}"));
-    v
+    let call = vec![format!("TODO: call ffi:{}()", name)];
+    let unsafe_ = format_block_smart("unsafe {", "}", &call, " ", " ");
+    let block = format_block("", "}", &unsafe_);
+    comment_block(&block)
 }
