75bbfb4c9a5cac258c94c19a5aa88e2c8cae741b
Gleb Kozyrev <gleb@gkoz.com>
2015-10-30 02:51:24 +0200

    Change the way tests are run
    Only test glib-sys for now but run its abi_tests

--- tests/sys/test.sh
+++ tests/sys/test.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
+LIBS="glib"
+MORE_LIBS=""
+
 GIR="cargo run --release --"
-FEATURES=""
 if [ "$1" = "full" ]; then
-	FEATURES="$FEATURES full"
+	LIBS="$LIBS $MORE_LIBS"
 fi
 export RUST_LOG="gir=warn"
 
 cd "`dirname $0`"
 cargo build --release || exit 1
 
-for TOML in gir-*.toml; do
-	$GIR -c ${TOML} || exit 2
+for LIB in $LIBS; do
+	$GIR -c "gir-${LIB}.toml" || exit 2
+	cd "${LIB}-sys"
+	cargo build || exit 3
+	cargo test --features abi_tests || exit 4
+	cd ..
 done
-
-cd sys_build
-cargo build --features "$FEATURES" || exit 3
-cd ..
