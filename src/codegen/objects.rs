e643dd05f64ec751cbb2d4380071c82980aa5a4e
Gleb Kozyrev <gleb@gkoz.com>
2016-01-05 17:16:56 +0200

    Add `module_name` config setting to allow setting the module names manually

--- src/codegen/objects.rs
+++ src/codegen/objects.rs
@@ -23,13 +23,17 @@ pub fn generate(env: &Env, root_path: &Path, mod_rs: &mut Vec<String>, traits: &
             }
         };
 
-        let path = root_path.join(file_name(&class_analysis.full_name));
+        let mod_name = obj.module_name.clone().unwrap_or_else(|| {
+            module_name(split_namespace_name(&class_analysis.full_name).1)
+        });
+
+        let mut path = root_path.join(&mod_name);
+        path.set_extension("rs");
         info!("Generating file {:?}", path);
 
         save_to_file(path, env.config.make_backup,
             |w| super::object::generate(w, env, &class_analysis));
 
-        let mod_name = module_name(split_namespace_name(&class_analysis.full_name).1);
         super::object::generate_reexports(env, &class_analysis, &mod_name, mod_rs, traits);
     }
 }
