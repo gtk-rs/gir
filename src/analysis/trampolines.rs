008bbaaa84295575c3654cdbbae448e18ef0e149
Evgenii Pashkin <eapashkin@gmail.com>
2016-03-26 14:57:26 +0300

    Name trampolines after signal

--- src/analysis/trampolines.rs
+++ src/analysis/trampolines.rs
@@ -1,5 +1,7 @@
 use env::Env;
 use library;
+use nameutil::signal_to_snake;
+use super::conversion_type::ConversionType;
 
 #[derive(Debug)]
 pub struct Trampoline {
@@ -11,57 +13,30 @@ pub struct Trampoline {
 pub type Trampolines = Vec<Trampoline>;
 
 pub fn analyze(env: &Env, signal: &library::Signal, trampolines: &mut Trampolines) -> Option<String> {
-    let name = generate_name(env, signal, trampolines);
-    if name.is_none() { return None }
+    if !can_generate(env, signal) {
+        return None;
+    }
+
+    let name = format!("{}_trampoline", signal_to_snake(&signal.name));
 
     let trampoline = Trampoline {
-        name: name.clone().unwrap(),
+        name: name.clone(),
         parameters: signal.parameters.clone(),
         ret: signal.ret.clone(),
     };
     trampolines.push(trampoline);
-    name
+    Some(name)
 }
 
-fn generate_name(env: &Env, signal: &library::Signal, trampolines: &mut Trampolines) -> Option<String> {
-    let ret_name = match name_from_return_type(env, &signal.ret) {
-        Some(name) => name,
-        None => return None,
-    };
-    let mut name = if signal.parameters.is_empty() {
-        Some(format!("{}_trampoline", ret_name))
-    } else {
-        //TODO: trampolines with params
-        None
-    };
-
-    if name.is_none() {
-        name = Some(format!("{}_unnamed_trampoline_{}", ret_name, trampolines.len() + 1));
-    }
-    name
-}
-
-fn name_from_return_type(env: &Env, ret: &library::Parameter) -> Option<String> {
-    use library::Type::*;
-    let some = |s: &str| Some(s.to_owned());
-    match *env.type_(ret.typ) {
-        Fundamental(fund) => {
-            use library::Fundamental;
-            match fund {
-                Fundamental::None => some("void"),
-                Fundamental::Boolean => some("bool"),
-                Fundamental::Int => some("int"),
-                _ => None,
-            }
+fn can_generate(env: &Env, signal: &library::Signal) -> bool {
+    if signal.ret.typ != Default::default() &&
+        ConversionType::of(&env.library, signal.ret.typ) == ConversionType::Unknown {
+            return false;
         }
-        _ => None
+    for par in &signal.parameters {
+        if ConversionType::of(&env.library, par.typ) == ConversionType::Unknown {
+            return false;
+        }
     }
-    /* TODO: other return types:
-    ComboBox:format-entry-text => return utf8
-    confirm-overwrite => return GtkFileChooserConfirmation
-    create-context => return Gdk.GLContext
-    create-window => return Notebook
-    create-custom-widget => return GObject.Object
-    Scale:format-value => return utf8
-    */
+    true
 }
