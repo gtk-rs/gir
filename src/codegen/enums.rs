2116eed3d785f7e8c84c3808d051481b266f3a3c
johncf <johncf@protonmail.com>
2017-06-14 18:25:10 +0530

    codegen::enums: dont collect configs

--- src/codegen/enums.rs
+++ src/codegen/enums.rs
@@ -1,19 +1,17 @@
 use analysis::namespaces;
 use codegen::general::version_condition_string;
-use config::gobjects::GObject;
 use env::Env;
 use library::*;
 use std::path::Path;
 
 pub fn generate(env: &Env, _root_path: &Path, mod_rs: &mut Vec<String>) {
-    let configs: Vec<&GObject> = env.config.objects.values()
+    let configs = env.config.objects.values()
         .filter(|c| {
             c.status.need_generate() &&
                 c.type_id.map_or(false, |tid| tid.ns_id == namespaces::MAIN)
-        })
-        .collect();
+        });
     mod_rs.push(String::from(""));
-    for config in &configs {
+    for config in configs {
         if let Type::Enumeration(ref enum_) = *env.library.type_(config.type_id.unwrap()) {
             if let Some (cfg) = version_condition_string(env, enum_.version, false, 0) {
                 mod_rs.push(cfg);
