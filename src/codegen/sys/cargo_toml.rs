067159bf3bde02a24cfec0fa795f6d3f039236a1
Evgenii Pashkin <eapashkin@gmail.com>
2017-06-26 19:27:07 +0300

    Add customization of crate names for external libraries

--- src/codegen/sys/cargo_toml.rs
+++ src/codegen/sys/cargo_toml.rs
@@ -51,7 +51,7 @@ fn fill_empty(root: &mut Table, env: &Env) {
 
     let deps = upsert_table(root, "dependencies");
     for ext_lib in &env.config.external_libraries {
-        let ext_package = format!("{}_sys", crate_name(ext_lib)).replace("_", "-");
+        let ext_package = format!("{}-sys", ext_lib.crate_name);
         let dep = upsert_table(deps, &*ext_package);
         set_string(dep, "path", format!("../{}", ext_package));
         set_string(dep, "version", "0.2.0");
---
+++ src/config/external_libraries.rs
@@ -0,0 +1,115 @@
+use toml;
+
+use super::error::*;
+use nameutil::crate_name;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct ExternalLibrary {
+    pub namespace: String,
+    pub crate_name: String,
+}
+
+pub fn read_external_libraries(toml: &toml::Value) -> Result<Vec<ExternalLibrary>> {
+    let mut external_libraries = match toml.lookup("options.external_libraries") {
+        Some(a) => {
+            try!(a.as_result_vec("options.external_libraries"))
+                .iter()
+                .filter_map(|v| v.as_str().map(String::from))
+                .map(|namespace| {
+                    ExternalLibrary {
+                        crate_name: crate_name(&namespace),
+                        namespace: namespace,
+                    }
+                })
+                .collect()
+        }
+        None => Vec::new(),
+    };
+    let custom_libs = toml.lookup("external_libraries").and_then(|v| v.as_table());
+    if let Some(custom_libs) = custom_libs {
+        for custom_lib in custom_libs {
+            let crate_name = custom_lib.0;
+            if let Some(namespace) = custom_lib.1.as_str() {
+                let lib = ExternalLibrary {
+                    namespace: namespace.to_owned(),
+                    crate_name: crate_name.clone(),
+                };
+                external_libraries.push(lib);
+            } else {
+                bail!("For external library \"{}\" namespace must be string", crate_name);
+            }
+
+        }
+    }
+
+    Ok(external_libraries)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use toml;
+
+    fn toml(input: &str) -> toml::Value {
+        let value = toml::from_str(&input);
+        assert!(value.is_ok());
+        value.unwrap()
+    }
+
+    #[test]
+    fn test_read_external_libraries() {
+        let toml = toml(
+            r#"
+[options]
+external_libraries = [
+   "GLib",
+   "Gdk",
+   "GdkPixbuf",
+]
+
+[external_libraries]
+coollib="CoolLib"
+other-lib="OtherLib"
+"#,
+        );
+        let libs = read_external_libraries(&toml).unwrap();
+
+        assert_eq!(
+            libs[0],
+            ExternalLibrary {
+                namespace: "GLib".to_owned(),
+                crate_name: "glib".to_owned(),
+            }
+        );
+        assert_eq!(
+            libs[1],
+            ExternalLibrary {
+                namespace: "Gdk".to_owned(),
+                crate_name: "gdk".to_owned(),
+            }
+        );
+        assert_eq!(
+            libs[2],
+            ExternalLibrary {
+                namespace: "GdkPixbuf".to_owned(),
+                crate_name: "gdk_pixbuf".to_owned(),
+            }
+        );
+        //Sorted alphabetically
+        assert_eq!(
+            libs[3],
+            ExternalLibrary {
+                namespace: "CoolLib".to_owned(),
+                crate_name: "coollib".to_owned(),
+            }
+        );
+        assert_eq!(
+            libs[4],
+            ExternalLibrary {
+                namespace: "OtherLib".to_owned(),
+                crate_name: "other-lib".to_owned(),
+            }
+        );
+    }
+
+}
