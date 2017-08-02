56465f85b8b40be87becba10585e0d0cf78bbb2f
Sebastian Dröge <sebastian@centricular.com>
2017-08-02 19:39:59 +0300

    Fix borrowed_box clippy warning in generated trampoline code
    https://github.com/rust-lang-nursery/rust-clippy/wiki#borrowed_box

--- src/codegen/trampoline.rs
+++ src/codegen/trampoline.rs
@@ -53,7 +53,7 @@ pub fn generate(
         ));
     }
     try!(writeln!(w, "\tcallback_guard!();"));
-    try!(writeln!(w, "\tlet f: &Box_<{}> = transmute(f);", func_str));
+    try!(writeln!(w, "\tlet f: &&({}) = transmute(f);", func_str));
     try!(transformation_vars(w, analysis));
     let call = trampoline_call_func(env, analysis, in_trait);
     try!(writeln!(w, "\t{}", call));
