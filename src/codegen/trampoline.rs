a61ccc16be2e0add02ec7b7953f55ee04317cbf5
Evgenii Pashkin <eapashkin@gmail.com>
2018-02-17 08:59:39 +0300

    Remove adding callback_guard

--- src/codegen/trampoline.rs
+++ src/codegen/trampoline.rs
@@ -51,7 +51,6 @@ pub fn generate(
             object_name
         ));
     }
-    try!(writeln!(w, "\tcallback_guard!();"));
     try!(writeln!(w, "\tlet f: &&({}) = transmute(f);", func_str));
     try!(transformation_vars(w, analysis));
     let call = trampoline_call_func(env, analysis, in_trait);
