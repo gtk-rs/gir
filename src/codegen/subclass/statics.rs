6ae1b232fa7491e5de90530b65e1204f14ee5647
Dirk Van Haerenborgh <vhdirk@gmail.com>
2018-06-02 23:38:24 +0200

    insert subclass dependencies

--- src/codegen/subclass/statics.rs
+++ src/codegen/subclass/statics.rs
@@ -4,25 +4,62 @@ use std::fs;
 use env::Env;
 use config::ExternalLibrary;
 use nameutil;
+use analysis::namespaces;
 
 
 // TODO: copied from sys
 pub fn generate_extern_crates(w: &mut Write, env: &Env) -> Result<()> {
     for library in &env.config.external_libraries {
+        try!(w.write_all(get_extern_crate_string_ffi(library).as_bytes()));
+    }
+    for library in &env.config.external_libraries {
         try!(w.write_all(get_extern_crate_string(library).as_bytes()));
+    }
+    for library in &env.config.external_libraries {
+
+        if library.crate_name == "glib"{
+            continue;
+        }
+
+        let ns = &env.namespaces[namespaces::MAIN];
+        if ns.crate_name == library.crate_name {
+            continue;
+        }
+
+        try!(w.write_all(get_extern_crate_string_subclass(library).as_bytes()));
     }
 
     Ok(())
 }
 
-// TODO: copied from sys
 fn get_extern_crate_string(library: &ExternalLibrary) -> String {
+
+    let mut m = "";
+    if library.crate_name == "glib"{
+        m = "#[macro_use]\n";
+    }
+
     format!(
-        "extern crate {}_sys as {};\n",
+        "{}extern crate {};\n",
+        m,
+        library.crate_name.replace("-", "_")
+    )
+}
+fn get_extern_crate_string_ffi(library: &ExternalLibrary) -> String {
+    format!(
+        "extern crate {}_sys as {}_ffi;\n",
         library.crate_name.replace("-", "_"),
         nameutil::crate_name(&library.namespace)
     )
 }
+
+fn get_extern_crate_string_subclass(library: &ExternalLibrary) -> String {
+    format!(
+        "#[macro_use]\nextern crate {}_subclass;\n",
+        library.crate_name.replace("-", "_")
+    )
+}
+
 
 // TODO: copied from sys
 pub fn include_custom_modules(w: &mut Write, env: &Env) -> Result<()> {
