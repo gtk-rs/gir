3bec7d42e63a850d338e3e44bdc3ab532a483dc1
Dirk Van Haerenborgh <vhdirk@gmail.com>
2018-06-03 21:47:13 +0200

    generate lib instead of mod

--- src/codegen/subclass/mod.rs
+++ src/codegen/subclass/mod.rs
@@ -18,14 +18,14 @@ pub fn generate(env: &Env) {
     info!("Generating subclasssing traits {:?}", env.config.target_path);
 
     let root_path = env.config.target_path.join("src").join("auto");
-    let mut mod_rs: Vec<String> = Vec::new();
+    let mut lib_rs: Vec<String> = Vec::new();
     let mut modules: Vec<String> = Vec::new();
 
     generate_single_version_file(env);
 
-    class_impls::generate(env, &root_path, &mut mod_rs);
+    class_impls::generate(env, &root_path, &mut lib_rs);
 
-    generate_mod_rs(env, &root_path, &mod_rs);
+    generate_lib_rs(env, &root_path, &lib_rs);
 
     // lib_::generate(env);
     // build::generate(env);
@@ -34,14 +34,14 @@ pub fn generate(env: &Env) {
 }
 
 
-pub fn generate_mod_rs(env: &Env, root_path: &Path, mod_rs: &[String]) {
+pub fn generate_lib_rs(env: &Env, root_path: &Path, lib_rs: &[String]) {
 
-    let path = root_path.join("mod.rs");
+    let path = root_path.join("lib.rs");
     save_to_file(path, env.config.make_backup, |w| {
         try!(general::start_comments(w, &env.config));
         try!(writeln!(w));
         try!(statics::generate_extern_crates(w, env));
         try!(writeln!(w));
-        general::write_vec(w, mod_rs)
+        general::write_vec(w, lib_rs)
     });
 }
