e7f334463b20d50d1c00d7a76ed114506df3bac3
Dirk Van Haerenborgh <vhdirk@gmail.com>
2018-07-04 20:20:07 +0200

    also store non-nullable pointers

--- src/codegen/subclass/trampoline_to_glib.rs
+++ src/codegen/subclass/trampoline_to_glib.rs
@@ -13,12 +13,9 @@ pub fn trampoline_to_glib(
     method: &virtual_methods::Info,
 ) -> String {
     use analysis::conversion_type::ConversionType::*;
-    use analysis::rust_type::rust_type;
-    use codegen::sys::ffi_type::ffi_type;
 
     let param_name = if parameter.name.len() > 0 { parameter.name.clone() } else {"ret".to_string()};
 
-    // TODO: handle out parameters
     match ConversionType::of(env, parameter.typ) {
         Direct => format!("rs_{}{}", param_name, parameter.trampoline_to_glib(env)),
         Scalar => format!("rs_{}{}", param_name, parameter.trampoline_to_glib(env)),
@@ -34,31 +31,12 @@ pub fn trampoline_to_glib(
                 let right = (if parameter.transfer == library::Transfer::None {
                     format!(r#"
     match rs_{param_name} {{
-        Some(t_{param_name}) => {{
-            let ret = t_{param_name}{to_glib};
-            unsafe extern "C" fn destroy_{param_name}(p: glib_ffi::gpointer){{
-                {rust_type}::from_glib_full(p as {c_type});
-            }};
-            gobject_ffi::g_object_set_qdata_full(gptr as *mut gobject_ffi::GObject,
-                glib_ffi::g_quark_from_string("rs_{object_name}_{method_name}_{param_name}".to_glib_none().0),
-                ret as *mut c_void,
-                Some(destroy_{param_name})
-            );
-            ret
-        }},
+        Some(t_{param_name}) => {to_glib_destroy},
         None => ptr::null{mut_str}()
     }}"#,
-                    object_name=object.module_name(env).unwrap_or(object.name.to_lowercase()),
-                    method_name=method.name,
-                    to_glib=to_glib_xxx( if parameter.transfer == library::Transfer::None {
-                        library::Transfer::Full
-                    }else{
-                        parameter.transfer
-                    }),
+                    to_glib_destroy=to_glib_with_destroy(parameter, env, object, method, &param_name),
                     mut_str=mut_str,
-                    param_name=param_name,
-                    rust_type=rust_type(env, parameter.typ).into_string(),
-                    c_type= ffi_type(env, parameter.typ, &parameter.c_type).into_string())
+                    param_name=param_name)
                 } else {
                     format!(
                         "match rs_{param_name} {{ Some(t_{param_name}) => t_{param_name}{to_glib}, None => ptr::null{mut_str}()}}",
@@ -70,12 +48,44 @@ pub fn trampoline_to_glib(
 
                 right
             } else {
-                format!("rs_{}{}", param_name, parameter.trampoline_to_glib(env))
+                to_glib_with_destroy(parameter, env, object, method, &param_name).to_owned()
             }
         }
         Borrow => format!("rs_{}{}", param_name, parameter.trampoline_to_glib(env)),
         Unknown => format!("rs_{}{}", param_name, parameter.trampoline_to_glib(env)),
     }
+}
+
+fn to_glib_with_destroy(parameter: &library::Parameter,
+                        env: &env::Env,
+                        object: &object::Info,
+                        method: &virtual_methods::Info,
+                        param_name: &String) -> String {
+    use analysis::rust_type::rust_type;
+    use codegen::sys::ffi_type::ffi_type;
+
+    format!(r#"{{
+        let ret = t_{param_name}{to_glib};
+        unsafe extern "C" fn destroy_{param_name}(p: glib_ffi::gpointer){{
+            {rust_type}::from_glib_full(p as {c_type});
+        }};
+        gobject_ffi::g_object_set_qdata_full(gptr as *mut gobject_ffi::GObject,
+            glib_ffi::g_quark_from_string("rs_{object_name}_{method_name}_{param_name}".to_glib_none().0),
+            ret as *mut c_void,
+            Some(destroy_{param_name})
+        );
+        ret
+    }}"#,
+    object_name=object.module_name(env).unwrap_or(object.name.to_lowercase()),
+    method_name=method.name,
+    to_glib=to_glib_xxx( if parameter.transfer == library::Transfer::None {
+        library::Transfer::Full
+    }else{
+        parameter.transfer
+    }),
+    param_name=param_name,
+    rust_type=rust_type(env, parameter.typ).into_string(),
+    c_type= ffi_type(env, parameter.typ, &parameter.c_type).into_string())
 }
 
 fn to_glib_xxx(transfer: library::Transfer) -> &'static str {
