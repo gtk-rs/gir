ead47474894042b5edc46fb4885ebaeccb72b2e6
Antoni Boucher <bouanto@zoho.com>
2019-10-20 14:04:46 -0400

    Fix formatting

--- src/git.rs
+++ src/git.rs
@@ -3,16 +3,12 @@ use std::process::Command;
 
 pub fn repo_hash<P: AsRef<Path>>(path: P) -> Option<String> {
     let git_path = path.as_ref().to_str();
-    let mut args =
-        match git_path {
-            Some(path) => vec!["-C", path],
-            None => vec![],
-        };
+    let mut args = match git_path {
+        Some(path) => vec!["-C", path],
+        None => vec![],
+    };
     args.extend(&["rev-parse", "--short", "HEAD"]);
-    let hash = String::from_utf8(Command::new("git")
-        .args(&args)
-        .output().ok()?
-        .stdout).ok()?;
+    let hash = String::from_utf8(Command::new("git").args(&args).output().ok()?.stdout).ok()?;
     let hash = hash.trim_end_matches('\n');
 
     if dirty(path) {
@@ -24,11 +20,10 @@ pub fn repo_hash<P: AsRef<Path>>(path: P) -> Option<String> {
 
 fn dirty<P: AsRef<Path>>(path: P) -> bool {
     let path = path.as_ref().to_str();
-    let mut args =
-        match path {
-            Some(path) => vec!["-C", path],
-            None => vec![],
-        };
+    let mut args = match path {
+        Some(path) => vec!["-C", path],
+        None => vec![],
+    };
     args.extend(&["ls-files", "-m"]);
     match Command::new("git").args(&args).output() {
         Ok(modified_files) => !modified_files.stdout.is_empty(),
