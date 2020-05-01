76f993687dcbcc4856f08d0dd8249fe41c6f2a36
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2020-05-01 15:56:54 +0200

    build: use system-deps in generated code
    We can remove all our custom code probing for deps.

--- src/codegen/sys/build.rs
+++ src/codegen/sys/build.rs
@@ -1,11 +1,7 @@
 use super::collect_versions;
 use crate::{codegen::general, env::Env, file_saver::save_to_file};
 use log::info;
-use regex::Regex;
-use std::{
-    io::{Result, Write},
-    path::Path,
-};
+use std::io::{Result, Write};
 
 pub fn generate(env: &Env) {
     info!(
@@ -42,12 +38,8 @@ fn generate_build_script(w: &mut dyn Write, env: &Env, split_build_rs: bool) -> 
         w,
         "{}",
         r##"#[cfg(not(feature = "dox"))]
-extern crate pkg_config;
+extern crate system_deps;
 
-#[cfg(not(feature = "dox"))]
-use pkg_config::{Config, Error};
-#[cfg(not(feature = "dox"))]
-use std::env;
 #[cfg(not(feature = "dox"))]
 use std::io::prelude::*;
 #[cfg(not(feature = "dox"))]
@@ -70,125 +62,13 @@ fn main() {} // prevent linking libraries to avoid documentation failure
 
 #[cfg(not(feature = "dox"))]
 fn main() {
-    if let Err(s) = find() {
+    if let Err(s) = system_deps::Config::new().probe() {
         let _ = writeln!(io::stderr(), "{}", s);
         process::exit(1);
     }
 }
-
-#[cfg(not(feature = "dox"))]
-fn find() -> Result<(), Error> {
 "##
-    )?;
-
-    let ns = env.namespaces.main();
-    let regex = Regex::new(r"^lib(.+)\.(so.*|dylib)$").expect("Regex failed");
-    let shared_libs: Vec<_> = ns
-        .shared_libs
-        .iter()
-        .map(|s| {
-            let lib_path = Path::new(s);
-            let lib_file_name = lib_path
-                .file_name()
-                .expect("A 'shared-library' in the GIR file has an invalid form")
-                .to_str()
-                .expect("Failed to convert OsStr to str");
-            regex.replace(lib_file_name, "\"$1\"")
-        })
-        .collect();
-
-    writeln!(
-        w,
-        "\tlet package_name = \"{}\";",
-        ns.package_name
-            .as_ref()
-            .expect("Package name doesn't exist")
-    )?;
-    writeln!(w, "\tlet shared_libs = [{}];", shared_libs.join(", "))?;
-    write!(w, "\tlet version = ")?;
-    if split_build_rs {
-        writeln!(w, "build_version::version();")?;
-    } else {
-        write_version(w, env, true)?;
-    }
-
-    writeln!(
-        w,
-        "{}",
-        r##"
-    if let Ok(inc_dir) = env::var("GTK_INCLUDE_DIR") {
-        println!("cargo:include={}", inc_dir);
-    }
-    if let Ok(lib_dir) = env::var("GTK_LIB_DIR") {
-        for lib_ in shared_libs.iter() {
-            println!("cargo:rustc-link-lib=dylib={}", lib_);
-        }
-        println!("cargo:rustc-link-search=native={}", lib_dir);
-        return Ok(())
-    }
-
-    let target = env::var("TARGET").expect("TARGET environment variable doesn't exist");
-    let hardcode_shared_libs = target.contains("windows");
-
-    let mut config = Config::new();
-    config.atleast_version(version);
-    config.print_system_libs(false);
-    if hardcode_shared_libs {
-        config.cargo_metadata(false);
-    }
-    match config.probe(package_name) {
-        Ok(library) => {
-            if let Ok(paths) = std::env::join_paths(library.include_paths) {
-                println!("cargo:include={}", paths.to_string_lossy());
-            }
-            if hardcode_shared_libs {
-                for lib_ in shared_libs.iter() {
-                    println!("cargo:rustc-link-lib=dylib={}", lib_);
-                }
-                for path in library.link_paths.iter() {
-                    println!("cargo:rustc-link-search=native={}",
-                             path.to_str().expect("library path doesn't exist"));
-                }
-            }
-            Ok(())
-        }
-        Err(Error::EnvNoPkgConfig(_)) | Err(Error::Command { .. }) => {
-            for lib_ in shared_libs.iter() {
-                println!("cargo:rustc-link-lib=dylib={}", lib_);
-            }
-            Ok(())
-        }"##
-    )?;
-    if ns.name == "GObject" {
-        writeln!(
-            w,
-            "{}",
-            r##"        Err(err) => {
-            #[cfg(target_os = "macos")]
-            {
-                let _ = writeln!(
-                    io::stderr(),
-                    "Failed to run pkg-config\n\
-                     If you're using homebrew, try running `brew info libffi` and follow the instructions.\n\
-                     See https://github.com/Homebrew/homebrew-core/issues/40179 for more details\n"
-                );
-            }
-            Err(err)
-        }
-    }
-}
-"##
-        )
-    } else {
-        writeln!(
-            w,
-            "{}",
-            r##"        Err(err) => Err(err),
-    }
-}
-"##
-        )
-    }
+    )
 }
 
 fn generate_build_version(w: &mut dyn Write, env: &Env) -> Result<()> {
