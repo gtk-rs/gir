gtk-rs/gir

commit c63d1faefb2dbe4406ca01ac445a2b28d1aca0b5

Generate doc cfg attributes for more beautiful docs

src/codegen/general.rs
@@ -51,6 +51,11 @@ pub fn uses(w: &mut dyn Write, env: &Env, imports: &Imports) -> Result<()> {
                 "#[cfg(any({},feature = \"dox\"))]",
                 scope.constraints.join(", ")
             )?;
+            writeln!(
+                w,
+                "#[cfg_attr(feature = \"dox\", doc(cfg({})))]",
+                scope.constraints.join(", ")
+            )?;
         }
 
         version_condition(w, env, scope.version, false, 0)?;
@@ -517,7 +522,8 @@ pub fn version_condition_string(
         Some(v) if v > env.config.min_cfg_version => {
             let comment = if commented { "//" } else { "" };
             Some(format!(
-                "{}{}#[cfg(any({}, feature = \"dox\"))]",
+                "{0}{1}#[cfg(any({2}, feature = \"dox\"))]\n\
+                 {0}{1}#[cfg_attr(feature = \"dox\", doc(cfg({2})))]",
                 tabs(indent),
                 comment,
                 v.to_cfg()
@@ -536,7 +542,8 @@ pub fn not_version_condition(
     if let Some(v) = version {
         let comment = if commented { "//" } else { "" };
         let s = format!(
-            "{}{}#[cfg(any(not({}), feature = \"dox\"))]",
+            "{0}{1}#[cfg(any(not({2}), feature = \"dox\"))]\n\
+             {0}{1}#[cfg_attr(feature = \"dox\", doc(cfg(not({2}))))]",
             tabs(indent),
             comment,
             v.to_cfg()
@@ -587,7 +594,8 @@ pub fn cfg_condition_string(
         Some(v) => {
             let comment = if commented { "//" } else { "" };
             Some(format!(
-                "{}{}#[cfg(any({}, feature = \"dox\"))]",
+                "{0}{1}#[cfg(any({2}, feature = \"dox\"))]\n\
+                 {0}{1}#[cfg_attr(feature = \"dox\", doc(cfg({2})))]",
                 tabs(indent),
                 comment,
                 v

src/codegen/sys/functions.rs
@@ -254,6 +254,11 @@ fn generate_object_funcs(
         // since we work with gir-files from Linux, some function names need to be adjusted
         if is_windows_utf8 {
             writeln!(w, "    {}#[cfg(any(windows, feature = \"dox\"))]", comment)?;
+            writeln!(
+                w,
+                "    {}#[cfg_attr(feature = \"dox\", doc(cfg(windows)))]",
+                comment
+            )?;
             writeln!(w, "    {}pub fn {}_utf8{};", comment, name, sig)?;
             version_condition(w, env, version, commented, 1)?;
         }

src/codegen/sys/statics.rs
@@ -6,6 +6,7 @@ pub fn begin(w: &mut dyn Write) -> Result<()> {
         "",
         "#![allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]",
         "#![allow(clippy::approx_constant, clippy::type_complexity, clippy::unreadable_literal)]",
+        "#![cfg_attr(feature = \"dox\", feature(doc_cfg))]",
         "",
         "extern crate libc;",
     ];

commit a0a30960d746a325008e4a91a40e2cabc2ebd539

codegen/enum: Do not generate doc(cfg) on match arms
    warning: unused doc comment
    --> gstreamer/src/auto/enums.rs:761:41
        |
    761 |             #[cfg_attr(feature = "dox", doc(cfg(feature = "v1_18")))]
        |                                         ^^^^^^^^^^^^^^^^^^^^^^^^^^^
    762 |             66817 => EventType::InstantRateSyncTime,
        |             --------------------------------------- rustdoc does not generate documentation for match arms

Fixes: c63d1fa ("Generate doc cfg attributes for more beautiful docs")

src/codegen/enums.rs
@@ -1,7 +1,8 @@
 use crate::{
     analysis::{imports::Imports, namespaces},
     codegen::general::{
-        self, cfg_deprecated, derives, version_condition, version_condition_string,
+        self, cfg_deprecated, derives, version_condition, version_condition_no_doc,
+        version_condition_string,
     },
     config::gobjects::GObject,
     env::Env,
@@ -174,7 +175,7 @@ fn generate_enum(
             enum_.name
         )?;
         for member in &members {
-            version_condition(w, env, member.version, false, 3)?;
+            version_condition_no_doc(w, env, member.version, false, 3)?;
             writeln!(w, "\t\t\t{0}::{1} => \"{1}\",", enum_.name, member.name)?;
         }
         writeln!(
@@ -201,7 +202,7 @@ impl ToGlib for {name} {{
         ffi_name = enum_.c_type
     )?;
     for member in &members {
-        version_condition(w, env, member.version, false, 3)?;
+        version_condition_no_doc(w, env, member.version, false, 3)?;
         writeln!(
             w,
             "\t\t\t{}::{} => {}::{},",
@@ -238,7 +239,7 @@ impl FromGlib<{sys_crate_name}::{ffi_name}> for {name} {{
         assert = assert
     )?;
     for member in &members {
-        version_condition(w, env, member.version, false, 3)?;
+        version_condition_no_doc(w, env, member.version, false, 3)?;
         writeln!(
             w,
             "\t\t\t{} => {}::{},",
@@ -304,7 +305,7 @@ impl FromGlib<{sys_crate_name}::{ffi_name}> for {name} {{
         )?;
 
         for member in &members {
-            version_condition(w, env, member.version, false, 3)?;
+            version_condition_no_doc(w, env, member.version, false, 3)?;
             writeln!(
                 w,
                 "\t\t\t{} => Some({}::{}),",

src/codegen/general.rs
@@ -496,6 +496,24 @@ pub fn version_condition(
     Ok(())
 }
 
+pub fn version_condition_no_doc(
+    w: &mut dyn Write,
+    env: &Env,
+    version: Option<Version>,
+    commented: bool,
+    indent: usize,
+) -> Result<()> {
+    match version {
+        Some(v) if v > env.config.min_cfg_version => {
+            if let Some(s) = cfg_condition_string_no_doc(&Some(v.to_cfg()), commented, indent) {
+                writeln!(w, "{}", s)?
+            }
+        }
+        _ => {}
+    }
+    Ok(())
+}
+
 pub fn version_condition_string(
     env: &Env,
     version: Option<Version>,
@@ -554,6 +572,25 @@ pub fn cfg_condition(
         writeln!(w, "{}", s)?;
     }
     Ok(())
+}
+
+pub fn cfg_condition_string_no_doc(
+    cfg_condition: &Option<String>,
+    commented: bool,
+    indent: usize,
+) -> Option<String> {
+    match cfg_condition.as_ref() {
+        Some(v) => {
+            let comment = if commented { "//" } else { "" };
+            Some(format!(
+                "{0}{1}#[cfg(any({2}, feature = \"dox\"))]",
+                tabs(indent),
+                comment,
+                v
+            ))
+        }
+        None => None,
+    }
 }
 
 pub fn cfg_condition_string(