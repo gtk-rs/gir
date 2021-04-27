gtk-rs/gir

commit c6cacb0cb36c314c9a02126d1ddf3de0e18b1e41

Implement handling of new Value traits
See https://github.com/gtk-rs/gtk-rs/pull/454

src/analysis/properties.rs
@@ -214,6 +214,7 @@ fn analyze_property(
         }
         if type_string.is_ok() {
             imports.add("glib::StaticType");
+            imports.add("glib::ToValue");
         }
 
         Some(Property {
@@ -238,6 +239,9 @@ fn analyze_property(
     let setter = if writable {
         if let Ok(ref s) = used_rust_type(env, prop.typ, true) {
             imports.add_used_type(s);
+        }
+        if type_string.is_ok() {
+            imports.add("glib::ToValue");
         }
         let set_bound = PropertyBound::get(env, prop.typ);
         if type_string.is_ok() && set_bound.is_some() {

src/analysis/enums.rs
@@ -58,9 +58,8 @@ pub fn new(env: &Env, obj: &GObject, imports: &mut Imports) -> Option<Info> {
     if has_get_type {
         imports.add("glib::Type");
         imports.add("glib::StaticType");
-        imports.add("glib::value::SetValue");
         imports.add("glib::value::FromValue");
-        imports.add("glib::value::FromValueOptional");
+        imports.add("glib::value::ToValue");
     }
 
     if obj.generate_display_trait {

src/analysis/flags.rs
@@ -53,9 +53,8 @@ pub fn new(env: &Env, obj: &GObject, imports: &mut Imports) -> Option<Info> {
     if has_get_type {
         imports.add("glib::Type");
         imports.add("glib::StaticType");
-        imports.add("glib::value::SetValue");
         imports.add("glib::value::FromValue");
-        imports.add("glib::value::FromValueOptional");
+        imports.add("glib::value::ToValue");
     }
 
     if obj.generate_display_trait {

src/codegen/enums.rs
@@ -349,41 +349,52 @@ impl FromGlib<{sys_crate_name}::{ffi_name}> for {name} {{
         version_condition(w, env, version, false, 0)?;
         writeln!(
             w,
-            "impl<'a> FromValueOptional<'a> for {name} {{
-    unsafe fn from_value_optional(value: &{gvalue}) -> Option<Self> {{
-        Some(FromValue::from_value(value))
-    }}
+            "impl {valuetype} for {name} {{
+    type Type = Self;
 }}",
             name = enum_.name,
-            gvalue = use_glib_type(env, "Value"),
+            valuetype = use_glib_type(env, "value::ValueType"),
         )?;
         writeln!(w)?;
 
         version_condition(w, env, version, false, 0)?;
         writeln!(
             w,
-            "impl<'a> FromValue<'a> for {name} {{
-    unsafe fn from_value(value: &{gvalue}) -> Self {{
-        from_glib({glib}(value.to_glib_none().0))
+            "unsafe impl<'a> FromValue<'a> for {name} {{
+    type Checker = {genericwrongvaluetypechecker}<Self>;
+
+    unsafe fn from_value(value: &'a {gvalue}) -> Self {{
+        {assert}from_glib({glib}(value.to_glib_none().0))
     }}
 }}",
             name = enum_.name,
             glib = use_glib_type(env, "gobject_ffi::g_value_get_enum"),
             gvalue = use_glib_type(env, "Value"),
+            genericwrongvaluetypechecker = use_glib_type(env, "value::GenericValueTypeChecker"),
+            assert = assert,
         )?;
         writeln!(w)?;
 
         version_condition(w, env, version, false, 0)?;
         writeln!(
             w,
-            "impl SetValue for {name} {{
-    unsafe fn set_value(value: &mut {gvalue}, this: &Self) {{
-        {glib}(value.to_glib_none_mut().0, this.to_glib())
+            "impl ToValue for {name} {{
+    fn to_value(&self) -> {gvalue} {{
+        let mut value = {gvalue}::for_value_type::<{name}>();
+        unsafe {{
+            {glib}(value.to_glib_none_mut().0, self.to_glib());
+        }}
+        value
+    }}
+
+    fn value_type(&self) -> {gtype} {{
+        Self::static_type()
     }}
 }}",
             name = enum_.name,
             glib = use_glib_type(env, "gobject_ffi::g_value_set_enum"),
             gvalue = use_glib_type(env, "Value"),
+            gtype = use_glib_type(env, "Type"),
         )?;
         writeln!(w)?;
     }

src/codegen/flags.rs
@@ -204,43 +204,53 @@ impl FromGlib<{sys_crate_name}::{ffi_name}> for {name} {{
         version_condition(w, env, version, false, 0)?;
         writeln!(
             w,
-            "impl<'a> FromValueOptional<'a> for {name} {{
-    unsafe fn from_value_optional(value: &{gvalue}) -> Option<Self> {{
-        Some(FromValue::from_value(value))
-    }}
+            "impl {valuetype} for {name} {{
+    type Type = Self;
 }}",
             name = flags.name,
-            gvalue = use_glib_type(env, "Value"),
+            valuetype = use_glib_type(env, "value::ValueType"),
         )?;
         writeln!(w)?;
 
         version_condition(w, env, version, false, 0)?;
         writeln!(
             w,
-            "impl<'a> FromValue<'a> for {name} {{
-    unsafe fn from_value(value: &{gvalue}) -> Self {{
-        from_glib({glib}(value.to_glib_none().0))
+            "unsafe impl<'a> FromValue<'a> for {name} {{
+    type Checker = {genericwrongvaluetypechecker}<Self>;
+
+    unsafe fn from_value(value: &'a {gvalue}) -> Self {{
+        {assert}from_glib({glib}(value.to_glib_none().0))
     }}
 }}",
             name = flags.name,
             glib = use_glib_type(env, "gobject_ffi::g_value_get_flags"),
             gvalue = use_glib_type(env, "Value"),
+            genericwrongvaluetypechecker = use_glib_type(env, "value::GenericValueTypeChecker"),
+            assert = assert,
         )?;
         writeln!(w)?;
 
         version_condition(w, env, version, false, 0)?;
         writeln!(
             w,
-            "impl SetValue for {name} {{
-    unsafe fn set_value(value: &mut {gvalue}, this: &Self) {{
-        {glib}(value.to_glib_none_mut().0, this.to_glib())
+            "impl ToValue for {name} {{
+    fn to_value(&self) -> {gvalue} {{
+        let mut value = {gvalue}::for_value_type::<{name}>();
+        unsafe {{
+            {glib}(value.to_glib_none_mut().0, self.to_glib());
+        }}
+        value
+    }}
+
+    fn value_type(&self) -> {gtype} {{
+        Self::static_type()
     }}
 }}",
             name = flags.name,
             glib = use_glib_type(env, "gobject_ffi::g_value_set_flags"),
             gvalue = use_glib_type(env, "Value"),
+            gtype = use_glib_type(env, "Type"),
         )?;
-
         writeln!(w)?;
     }
 

src/codegen/property_body.rs
@@ -150,23 +150,9 @@ impl<'a> Builder<'a> {
             call: Box::new(ffi_call),
         });
 
-        let unwrap = if self.is_nullable {
-            // This one is strictly speaking nullable, but
-            // we represent that with an empty Vec instead
-            if ["Vec<GString>", "Vec<crate::GString>", "Vec<glib::GString>"]
-                .iter()
-                .any(|&x| x == self.type_)
-            {
-                ".unwrap()"
-            } else {
-                ""
-            }
-        } else {
-            ".unwrap()"
-        };
         body.push(Chunk::Custom(format!(
-            "value.get().expect(\"Return Value for property `{}` getter\"){}",
-            self.name, unwrap,
+            "value.get().expect(\"Return Value for property `{}` getter\")",
+            self.name,
         )));
 
         vec![Chunk::Unsafe(body)]
@@ -199,10 +185,9 @@ impl<'a> Builder<'a> {
             "b\"{}\\0\".as_ptr() as *const _",
             self.name
         )));
-        let ref_str = if self.is_ref { "" } else { "&" };
         params.push(Chunk::Custom(format!(
-            "glib::Value::from({}{}).to_glib_none().0",
-            ref_str, self.var_name
+            "{}.to_value().to_glib_none().0",
+            self.var_name
         )));
 
         let mut body = Vec::new();

src/codegen/signal.rs
@@ -8,7 +8,6 @@ use crate::{
     chunk::Chunk,
     consts::TYPE_PARAMETERS_START,
     env::Env,
-    library,
     nameutil::use_glib_type,
     writer::{primitives::tabs, ToCode},
 };
@@ -139,18 +138,11 @@ pub fn generate(
             )?;
 
             if trampoline.ret.typ != Default::default() {
-                let unwrap = if trampoline.ret.nullable == library::Nullable(true) {
-                    ""
-                } else {
-                    ".unwrap()"
-                };
-
                 writeln!(
                     w,
-                    "{}res.unwrap().get().expect(\"Return Value for `{}`\"){}",
+                    "{}res.unwrap().get().expect(\"Return Value for `{}`\")",
                     tabs(indent + 1),
                     emit_name,
-                    unwrap,
                 )?;
             }
             writeln!(w, "{}}}", tabs(indent))?;

commit 3698e6a3258687800ba3414da42be24d511bda4f

analysis/properties: Remove unused ToValue import for property getters
Following [1] an unnecessary `glib::ToValue` import showed up in auto
code, which does not appear to be used in gtk-rs nor gstreamer-rs.
After all, getters convert from Values, not to Values.

Fixes: c6cacb0 ("Implement handling of new Value traits")

[1]: https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/merge_requests/746#note_893561

src/analysis/properties.rs
@@ -214,7 +214,6 @@ fn analyze_property(
         }
         if type_string.is_ok() {
             imports.add("glib::StaticType");
-            imports.add("glib::ToValue");
         }
 
         Some(Property {