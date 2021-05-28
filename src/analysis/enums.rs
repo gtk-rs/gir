gtk-rs/gir

commit 50cee9e04e3b1681beefe37abb47032816120742

enums: Move rejection and glib imports from codegen to analysis
This not only cleans up unnecessary mutable variables in the codegen
loop and a cloned Imports instance (so that it can remain immutable
during codegen), it also adds the missing rejection logic to enum
analysis.  No harm was caused because these checks happened at codegen
prior, but they omit some unnecessary imports.

src/analysis/enums.rs
@@ -29,6 +29,17 @@ impl Info {
 pub fn new(env: &Env, obj: &GObject, imports: &mut Imports) -> Option<Info> {
     info!("Analyzing enumeration {}", obj.name);
 
+    if !obj.status.need_generate() {
+        return None;
+    }
+
+    if !obj
+        .type_id
+        .map_or(false, |tid| tid.ns_id == namespaces::MAIN)
+    {
+        return None;
+    }
+
     let enumeration_tid = env.library.find_type(0, &obj.name)?;
     let type_ = env.type_(enumeration_tid);
     let enumeration: &library::Enumeration = type_.maybe_ref()?;
@@ -37,6 +48,25 @@ pub fn new(env: &Env, obj: &GObject, imports: &mut Imports) -> Option<Info> {
 
     // Mark the type as available within the enum namespace:
     imports.add_defined(&format!("crate::{}", name));
+
+    let has_get_quark = enumeration.error_domain.is_some();
+    if has_get_quark {
+        imports.add("glib::Quark");
+        imports.add("glib::error::ErrorDomain");
+    }
+
+    let has_get_type = enumeration.glib_get_type.is_some();
+    if has_get_type {
+        imports.add("glib::Type");
+        imports.add("glib::StaticType");
+        imports.add("glib::value::SetValue");
+        imports.add("glib::value::FromValue");
+        imports.add("glib::value::FromValueOptional");
+    }
+
+    if obj.generate_display_trait {
+        imports.add("std::fmt");
+    }
 
     let mut functions = functions::analyze(
         env,

src/analysis/mod.rs
@@ -65,6 +65,7 @@ pub fn run(env: &mut Env) {
     }
 
     let mut enum_imports = Imports::new(&env.library);
+    enum_imports.add("glib::translate::*");
 
     let mut analyzed = 1;
     while analyzed > 0 {

src/codegen/enums.rs
@@ -1,7 +1,6 @@
 use super::function;
 use crate::{
     analysis::enums::Info,
-    analysis::namespaces,
     codegen::general::{
         self, cfg_deprecated, derives, version_condition, version_condition_no_doc,
         version_condition_string,
@@ -21,72 +20,19 @@ use std::{
 };
 
 pub fn generate(env: &Env, root_path: &Path, mod_rs: &mut Vec<String>) {
-    fn should_keep(c: &GObject) -> bool {
-        c.status.need_generate() && c.type_id.map_or(false, |tid| tid.ns_id == namespaces::MAIN)
-    }
-
-    let mut has_get_quark = false;
-    let mut has_any = false;
-    let mut has_get_type = false;
-    let mut generate_display_trait = false;
-
-    for enum_analysis in &env.analysis.enumerations {
-        let config = &env.config.objects[&enum_analysis.full_name];
-        if !should_keep(config) {
-            continue;
-        }
-
-        let enum_ = enum_analysis.type_(&env.library);
-        has_any = true;
-        if enum_.error_domain.is_some() {
-            has_get_quark = true;
-        }
-        if enum_.glib_get_type.is_some() {
-            has_get_type = true;
-        }
-        generate_display_trait |= config.generate_display_trait;
-
-        if has_get_type && has_get_quark {
-            break;
-        }
-    }
-
-    if !has_any {
+    if env.analysis.enumerations.is_empty() {
         return;
-    }
-
-    // TODO: We should do this just once in analysis without mutation necessary afterwards
-    let mut imports = env.analysis.enum_imports.clone();
-
-    if has_get_quark {
-        imports.add("glib::Quark");
-        imports.add("glib::error::ErrorDomain");
-    }
-    if has_get_type {
-        imports.add("glib::Type");
-        imports.add("glib::StaticType");
-        imports.add("glib::value::SetValue");
-        imports.add("glib::value::FromValue");
-        imports.add("glib::value::FromValueOptional");
-    }
-    imports.add("glib::translate::*");
-
-    if generate_display_trait {
-        imports.add("std::fmt");
     }
 
     let path = root_path.join("enums.rs");
     file_saver::save_to_file(path, env.config.make_backup, |w| {
         general::start_comments(w, &env.config)?;
-        general::uses(w, env, &imports)?;
+        general::uses(w, env, &env.analysis.enum_imports)?;
         writeln!(w)?;
 
         mod_rs.push("\nmod enums;".into());
         for enum_analysis in &env.analysis.enumerations {
             let config = &env.config.objects[&enum_analysis.full_name];
-            if !should_keep(config) {
-                continue;
-            }
             let enum_ = enum_analysis.type_(&env.library);
 
             if let Some(cfg) = version_condition_string(env, enum_.version, false, 0) {

commit 68591f18a6b9670b20dddf96ef512a0ac17cedf0

enums,flags: Always analyze manual types
Docs generation is going to look at analysis data to know what variants
and functions are available on enums/flags.  External types (in the
`manual = []` list) were previously omitted because of their status.
This changes enums and flags to behave like objects and records which
only bail if the type is entirely ignored, and perform a
`need_generate()` check at `codegen` instead.

Fixes: 50cee9e ("enums: Move rejection and glib imports from codegen to analysis")
Fixes: e549975 ("Copypaste enum analysis and function generation to bitfield")

src/analysis/enums.rs
@@ -25,14 +25,7 @@ impl Info {
 pub fn new(env: &Env, obj: &GObject, imports: &mut Imports) -> Option<Info> {
     info!("Analyzing enumeration {}", obj.name);
 
-    if !obj.status.need_generate() {
-        return None;
-    }
-
-    if !obj
-        .type_id
-        .map_or(false, |tid| tid.ns_id == namespaces::MAIN)
-    {
+    if obj.status.ignored() {
         return None;
     }
 
@@ -42,28 +35,30 @@ pub fn new(env: &Env, obj: &GObject, imports: &mut Imports) -> Option<Info> {
 
     let name = split_namespace_name(&obj.name).1;
 
-    // Mark the type as available within the enum namespace:
-    imports.add_defined(&format!("crate::{}", name));
+    if obj.status.need_generate() {
+        // Mark the type as available within the enum namespace:
+        imports.add_defined(&format!("crate::{}", name));
 
-    let imports = &mut imports.with_defaults(enumeration.version, &None);
-    imports.add("glib::translate::*");
+        let imports = &mut imports.with_defaults(enumeration.version, &None);
+        imports.add("glib::translate::*");
 
-    let has_get_quark = enumeration.error_domain.is_some();
-    if has_get_quark {
-        imports.add("glib::Quark");
-        imports.add("glib::error::ErrorDomain");
-    }
+        let has_get_quark = enumeration.error_domain.is_some();
+        if has_get_quark {
+            imports.add("glib::Quark");
+            imports.add("glib::error::ErrorDomain");
+        }
 
-    let has_get_type = enumeration.glib_get_type.is_some();
-    if has_get_type {
-        imports.add("glib::Type");
-        imports.add("glib::StaticType");
-        imports.add("glib::value::FromValue");
-        imports.add("glib::value::ToValue");
-    }
+        let has_get_type = enumeration.glib_get_type.is_some();
+        if has_get_type {
+            imports.add("glib::Type");
+            imports.add("glib::StaticType");
+            imports.add("glib::value::FromValue");
+            imports.add("glib::value::ToValue");
+        }
 
-    if obj.generate_display_trait {
-        imports.add("std::fmt");
+        if obj.generate_display_trait {
+            imports.add("std::fmt");
+        }
     }
 
     let mut functions = functions::analyze(

src/analysis/flags.rs
@@ -25,14 +25,7 @@ impl Info {
 pub fn new(env: &Env, obj: &GObject, imports: &mut Imports) -> Option<Info> {
     info!("Analyzing flags {}", obj.name);
 
-    if !obj.status.need_generate() {
-        return None;
-    }
-
-    if !obj
-        .type_id
-        .map_or(false, |tid| tid.ns_id == namespaces::MAIN)
-    {
+    if obj.status.ignored() {
         return None;
     }
 
@@ -42,23 +35,25 @@ pub fn new(env: &Env, obj: &GObject, imports: &mut Imports) -> Option<Info> {
 
     let name = split_namespace_name(&obj.name).1;
 
-    // Mark the type as available within the bitfield namespace:
-    imports.add_defined(&format!("crate::{}", name));
+    if obj.status.need_generate() {
+        // Mark the type as available within the bitfield namespace:
+        imports.add_defined(&format!("crate::{}", name));
 
-    let imports = &mut imports.with_defaults(flags.version, &None);
-    imports.add("glib::translate::*");
-    imports.add("bitflags::bitflags");
+        let imports = &mut imports.with_defaults(flags.version, &None);
+        imports.add("glib::translate::*");
+        imports.add("bitflags::bitflags");
 
-    let has_get_type = flags.glib_get_type.is_some();
-    if has_get_type {
-        imports.add("glib::Type");
-        imports.add("glib::StaticType");
-        imports.add("glib::value::FromValue");
-        imports.add("glib::value::ToValue");
-    }
+        let has_get_type = flags.glib_get_type.is_some();
+        if has_get_type {
+            imports.add("glib::Type");
+            imports.add("glib::StaticType");
+            imports.add("glib::value::FromValue");
+            imports.add("glib::value::ToValue");
+        }
 
-    if obj.generate_display_trait {
-        imports.add("std::fmt");
+        if obj.generate_display_trait {
+            imports.add("std::fmt");
+        }
     }
 
     let mut functions = functions::analyze(

src/analysis/mod.rs
@@ -49,6 +49,7 @@ pub struct Analysis {
     pub records: BTreeMap<String, record::Info>,
     pub global_functions: Option<info_base::InfoBase>,
     pub constants: Vec<constants::Info>,
+
     pub enumerations: Vec<enums::Info>,
     pub enum_imports: Imports,
 

src/codegen/enums.rs
@@ -21,7 +21,12 @@ use std::{
 };
 
 pub fn generate(env: &Env, root_path: &Path, mod_rs: &mut Vec<String>) {
-    if env.analysis.enumerations.is_empty() {
+    if !env
+        .analysis
+        .enumerations
+        .iter()
+        .any(|e| env.config.objects[&e.full_name].status.need_generate())
+    {
         return;
     }
 
@@ -34,6 +39,10 @@ pub fn generate(env: &Env, root_path: &Path, mod_rs: &mut Vec<String>) {
         mod_rs.push("\nmod enums;".into());
         for enum_analysis in &env.analysis.enumerations {
             let config = &env.config.objects[&enum_analysis.full_name];
+            if !config.status.need_generate() {
+                continue;
+            }
+
             let enum_ = enum_analysis.type_(&env.library);
 
             if let Some(cfg) = version_condition_string(env, enum_.version, false, 0) {

src/codegen/flags.rs
@@ -18,7 +18,12 @@ use std::{
 };
 
 pub fn generate(env: &Env, root_path: &Path, mod_rs: &mut Vec<String>) {
-    if env.analysis.flags.is_empty() {
+    if !env
+        .analysis
+        .flags
+        .iter()
+        .any(|f| env.config.objects[&f.full_name].status.need_generate())
+    {
         return;
     }
 
@@ -31,6 +36,9 @@ pub fn generate(env: &Env, root_path: &Path, mod_rs: &mut Vec<String>) {
         mod_rs.push("\nmod flags;".into());
         for flags_analysis in &env.analysis.flags {
             let config = &env.config.objects[&flags_analysis.full_name];
+            if !config.status.need_generate() {
+                continue;
+            }
             let flags = flags_analysis.type_(&env.library);
 
             if let Some(cfg) = version_condition_string(env, flags.version, false, 0) {