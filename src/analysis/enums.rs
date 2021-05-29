gtk-rs/gir

commit 68591f18a6b9670b20dddf96ef512a0ac17cedf0

enums,flags: Always analyze manual types
Docs generation is going to look at analysis data to know what variants
and functions are available on enums/flags.  External types (in the
`manual = []` list) were previously omitted because of their status.
This changes enums and flags to behave like objects and records which
only bail if the type is entirely ignored, and perform a
`need_generate()` check at `codegen` instead.

Fixes: 50cee9e ("enums: Move rejection and glib imports from codegen to analysis")
Fixes: e549975 ("Copypaste enum analysis and function generation to bitfield")

src/analysis/enums.rs
@@ -25,14 +25,7 @@ impl Info {
 pub fn new(env: &Env, obj: &GObject, imports: &mut Imports) -> Option<Info> {
     info!("Analyzing enumeration {}", obj.name);
 
-    if !obj.status.need_generate() {
-        return None;
-    }
-
-    if !obj
-        .type_id
-        .map_or(false, |tid| tid.ns_id == namespaces::MAIN)
-    {
+    if obj.status.ignored() {
         return None;
     }
 
@@ -42,28 +35,30 @@ pub fn new(env: &Env, obj: &GObject, imports: &mut Imports) -> Option<Info> {
 
     let name = split_namespace_name(&obj.name).1;
 
-    // Mark the type as available within the enum namespace:
-    imports.add_defined(&format!("crate::{}", name));
+    if obj.status.need_generate() {
+        // Mark the type as available within the enum namespace:
+        imports.add_defined(&format!("crate::{}", name));
 
-    let imports = &mut imports.with_defaults(enumeration.version, &None);
-    imports.add("glib::translate::*");
+        let imports = &mut imports.with_defaults(enumeration.version, &None);
+        imports.add("glib::translate::*");
 
-    let has_get_quark = enumeration.error_domain.is_some();
-    if has_get_quark {
-        imports.add("glib::Quark");
-        imports.add("glib::error::ErrorDomain");
-    }
+        let has_get_quark = enumeration.error_domain.is_some();
+        if has_get_quark {
+            imports.add("glib::Quark");
+            imports.add("glib::error::ErrorDomain");
+        }
 
-    let has_get_type = enumeration.glib_get_type.is_some();
-    if has_get_type {
-        imports.add("glib::Type");
-        imports.add("glib::StaticType");
-        imports.add("glib::value::FromValue");
-        imports.add("glib::value::ToValue");
-    }
+        let has_get_type = enumeration.glib_get_type.is_some();
+        if has_get_type {
+            imports.add("glib::Type");
+            imports.add("glib::StaticType");
+            imports.add("glib::value::FromValue");
+            imports.add("glib::value::ToValue");
+        }
 
-    if obj.generate_display_trait {
-        imports.add("std::fmt");
+        if obj.generate_display_trait {
+            imports.add("std::fmt");
+        }
     }
 
     let mut functions = functions::analyze(

src/analysis/flags.rs
@@ -25,14 +25,7 @@ impl Info {
 pub fn new(env: &Env, obj: &GObject, imports: &mut Imports) -> Option<Info> {
     info!("Analyzing flags {}", obj.name);
 
-    if !obj.status.need_generate() {
-        return None;
-    }
-
-    if !obj
-        .type_id
-        .map_or(false, |tid| tid.ns_id == namespaces::MAIN)
-    {
+    if obj.status.ignored() {
         return None;
     }
 
@@ -42,23 +35,25 @@ pub fn new(env: &Env, obj: &GObject, imports: &mut Imports) -> Option<Info> {
 
     let name = split_namespace_name(&obj.name).1;
 
-    // Mark the type as available within the bitfield namespace:
-    imports.add_defined(&format!("crate::{}", name));
+    if obj.status.need_generate() {
+        // Mark the type as available within the bitfield namespace:
+        imports.add_defined(&format!("crate::{}", name));
 
-    let imports = &mut imports.with_defaults(flags.version, &None);
-    imports.add("glib::translate::*");
-    imports.add("bitflags::bitflags");
+        let imports = &mut imports.with_defaults(flags.version, &None);
+        imports.add("glib::translate::*");
+        imports.add("bitflags::bitflags");
 
-    let has_get_type = flags.glib_get_type.is_some();
-    if has_get_type {
-        imports.add("glib::Type");
-        imports.add("glib::StaticType");
-        imports.add("glib::value::FromValue");
-        imports.add("glib::value::ToValue");
-    }
+        let has_get_type = flags.glib_get_type.is_some();
+        if has_get_type {
+            imports.add("glib::Type");
+            imports.add("glib::StaticType");
+            imports.add("glib::value::FromValue");
+            imports.add("glib::value::ToValue");
+        }
 
-    if obj.generate_display_trait {
-        imports.add("std::fmt");
+        if obj.generate_display_trait {
+            imports.add("std::fmt");
+        }
     }
 
     let mut functions = functions::analyze(

src/codegen/enums.rs
@@ -21,7 +21,12 @@ use std::{
 };
 
 pub fn generate(env: &Env, root_path: &Path, mod_rs: &mut Vec<String>) {
-    if env.analysis.enumerations.is_empty() {
+    if !env
+        .analysis
+        .enumerations
+        .iter()
+        .any(|e| env.config.objects[&e.full_name].status.need_generate())
+    {
         return;
     }
 
@@ -34,6 +39,10 @@ pub fn generate(env: &Env, root_path: &Path, mod_rs: &mut Vec<String>) {
         mod_rs.push("\nmod enums;".into());
         for enum_analysis in &env.analysis.enumerations {
             let config = &env.config.objects[&enum_analysis.full_name];
+            if !config.status.need_generate() {
+                continue;
+            }
+
             let enum_ = enum_analysis.type_(&env.library);
 
             if let Some(cfg) = version_condition_string(env, enum_.version, false, 0) {

src/codegen/flags.rs
@@ -18,7 +18,12 @@ use std::{
 };
 
 pub fn generate(env: &Env, root_path: &Path, mod_rs: &mut Vec<String>) {
-    if env.analysis.flags.is_empty() {
+    if !env
+        .analysis
+        .flags
+        .iter()
+        .any(|f| env.config.objects[&f.full_name].status.need_generate())
+    {
         return;
     }
 
@@ -31,6 +36,9 @@ pub fn generate(env: &Env, root_path: &Path, mod_rs: &mut Vec<String>) {
         mod_rs.push("\nmod flags;".into());
         for flags_analysis in &env.analysis.flags {
             let config = &env.config.objects[&flags_analysis.full_name];
+            if !config.status.need_generate() {
+                continue;
+            }
             let flags = flags_analysis.type_(&env.library);
 
             if let Some(cfg) = version_condition_string(env, flags.version, false, 0) {

src/analysis/mod.rs
@@ -49,6 +49,7 @@ pub struct Analysis {
     pub records: BTreeMap<String, record::Info>,
     pub global_functions: Option<info_base::InfoBase>,
     pub constants: Vec<constants::Info>,
+
     pub enumerations: Vec<enums::Info>,
     pub enum_imports: Imports,
 

commit 6c2c8c2a8a10563f3c71a76f1bb072856d0a397d

enums,flags: Do not analyze imports if the type is not generated
Otherwise unused imports show up in the enums/flags files.

Fixes: 68591f1 ("enums,flags: Always analyze manual types")

src/analysis/enums.rs
@@ -106,7 +106,9 @@ pub fn new(env: &Env, obj: &GObject, imports: &mut Imports) -> Option<Info> {
 
     let specials = special_functions::extract(&mut functions, type_, obj);
 
-    special_functions::analyze_imports(&specials, imports);
+    if obj.status.need_generate() {
+        special_functions::analyze_imports(&specials, imports);
+    }
 
     let info = Info {
         full_name: obj.name.clone(),

src/analysis/flags.rs
@@ -101,7 +101,9 @@ pub fn new(env: &Env, obj: &GObject, imports: &mut Imports) -> Option<Info> {
 
     let specials = special_functions::extract(&mut functions, type_, obj);
 
-    special_functions::analyze_imports(&specials, imports);
+    if obj.status.need_generate() {
+        special_functions::analyze_imports(&specials, imports);
+    }
 
     let info = Info {
         full_name: obj.name.clone(),