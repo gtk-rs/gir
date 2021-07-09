0038abc1c281bf9d94dcbcee8a56af9489878f16
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-07-10 00:09:23 +0200

    docs: disable language warning for various languages

--- src/codegen/doc/format.rs
+++ src/codegen/doc/format.rs
@@ -107,7 +107,9 @@ fn format(
                 ret.push_str(&replace_symbols(before, env, in_type));
 
                 let (lang, after) = get_markdown_language(after);
-                if !["text", "rust"].contains(&lang) && after.lines().count() > 1 {
+                if !["text", "rust", "xml", "css", "json", "html"].contains(&lang)
+                    && after.lines().count() > 1
+                {
                     ret.push_str(&format!("**⚠️ The following code is in {0} ⚠️**\n\n", lang));
                 }
                 ret.push_str(&format!("```{}\n", lang));
