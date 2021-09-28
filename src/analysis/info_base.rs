ae8892170a086c00e704f463e011e8886c3bea3c
Sebastian Dröge <sebastian@centricular.com>
2021-09-28 14:02:37 +0300

    Remove type version heuristics
    Previously we would infer the minimum version of a type by the minimum
    version of all its functions if not type version information was
    available. This causes unnecessarily high minimum version constraints if
    the type was available without functions before and later got functions.
    
    Instead this should be fixed in the C library's annotations, and for now
    this can also be overridden via Gir.toml if needed.

--- src/analysis/info_base.rs
+++ src/analysis/info_base.rs
@@ -1,6 +1,5 @@
-use super::{functions::Visibility, imports::Imports, *};
-use crate::{config::gobjects::GObject, env::Env, library, version::Version};
-use std::cmp;
+use super::{imports::Imports, *};
+use crate::{library, version::Version};
 
 #[derive(Debug, Default)]
 pub struct InfoBase {
@@ -38,42 +37,4 @@ impl InfoBase {
             .filter(|f| f.status.need_generate() && f.kind == library::FunctionKind::Function)
             .collect()
     }
-}
-
-pub fn versions(
-    env: &Env,
-    obj: &GObject,
-    functions: &[functions::Info],
-    version: Option<Version>,
-    deprecated_version: Option<Version>,
-) -> (Option<Version>, Option<Version>) {
-    let version = if obj.version.is_some() {
-        obj.version
-    } else {
-        let fn_version = functions
-            .iter()
-            .filter(|f| f.visibility == Visibility::Public)
-            .map(|f| f.version)
-            .min()
-            .unwrap_or(None);
-        cmp::max(version, fn_version)
-    };
-    let version = env.config.filter_version(version);
-
-    let fn_deprecated_max = functions
-        .iter()
-        .filter(|f| f.visibility == Visibility::Public)
-        .map(|f| f.deprecated_version)
-        .max()
-        .unwrap_or(None);
-    let fn_deprecated_min = functions
-        .iter()
-        .filter(|f| f.visibility == Visibility::Public)
-        .map(|f| f.deprecated_version)
-        .min()
-        .unwrap_or(None);
-    let deprecated_version =
-        deprecated_version.or_else(|| fn_deprecated_min.and(fn_deprecated_max));
-
-    (version, deprecated_version)
 }
