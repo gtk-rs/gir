gtk-rs/gir

commit e3bd76a34445c316d66f74e320c5262ccb160ca5

replace FunctionVisibility with hidden/commented attributes

src/codegen/special_functions.rs
@@ -1,7 +1,7 @@
 use std::io::{Result, Write};
 
 use crate::{
-    analysis::{self, functions::FuncVisibility, special_functions::FunctionType},
+    analysis::{self, special_functions::FunctionType},
     version::Version,
     Env,
 };
@@ -37,11 +37,6 @@ pub(super) fn generate_static_to_str(
     let version = Version::if_stricter_than(function.version, scope_version);
     version_condition(w, env, None, version, false, 1)?;
 
-    let visibility = match function.func_visibility {
-        FuncVisibility::Public => "pub ",
-        _ => "",
-    };
-
     writeln!(
         w,
         "\
@@ -56,7 +51,7 @@ pub(super) fn generate_static_to_str(
 \t\t\t.expect(\"{glib_fn_name} returned an invalid string\")
 \t\t}}
 \t}}",
-        visibility = visibility,
+        visibility = function.visibility,
         rust_fn_name = function.codegen_name(),
         ns = env.main_sys_crate_name(),
         glib_fn_name = function.glib_name,

src/analysis/special_functions.rs
@@ -1,8 +1,6 @@
 use crate::{
-    analysis::{
-        functions::{FuncVisibility, Info as FuncInfo},
-        imports::Imports,
-    },
+    analysis::{functions::Info as FuncInfo, imports::Imports},
+    codegen::Visibility,
     config::GObject,
     library::{Type as LibType, TypeId},
     version::Version,
@@ -124,8 +122,13 @@ fn is_stringify(func: &mut FuncInfo, parent_type: &LibType, obj: &GObject) -> bo
 }
 
 fn update_func(func: &mut FuncInfo, type_: Type) -> bool {
-    if func.func_visibility != FuncVisibility::Comment {
-        func.func_visibility = visibility(type_);
+    if !func.commented {
+        use self::Type::*;
+        match type_ {
+            Copy | Free | Ref | Unref => func.hidden = true,
+            Hash | Compare | Equal => func.visibility = Visibility::Private,
+            Display => func.visibility = Visibility::Public,
+        };
     }
     true
 }
@@ -226,23 +229,15 @@ pub fn extract(functions: &mut Vec<FuncInfo>, parent_type: &LibType, obj: &GObje
     specials
 }
 
-fn visibility(t: Type) -> FuncVisibility {
-    use self::Type::*;
-    match t {
-        Copy | Free | Ref | Unref => FuncVisibility::Hidden,
-        Hash | Compare | Equal => FuncVisibility::Private,
-        Display => FuncVisibility::Public,
-    }
-}
-
 // Some special functions (e.g. `copy` on refcounted types) should be exposed
 pub fn unhide(functions: &mut Vec<FuncInfo>, specials: &Infos, type_: Type) {
     if let Some(func) = specials.traits().get(&type_) {
-        let func = functions.iter_mut().find(|f| {
-            f.glib_name == func.glib_name && f.func_visibility != FuncVisibility::Comment
-        });
+        let func = functions
+            .iter_mut()
+            .find(|f| f.glib_name == func.glib_name && !f.commented);
         if let Some(func) = func {
-            func.func_visibility = FuncVisibility::Public;
+            func.visibility = Visibility::Public;
+            func.hidden = false;
         }
     }
 }

src/analysis/functions.rs
@@ -36,24 +36,6 @@ use std::{
 
 use super::special_functions;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum FuncVisibility {
-    Public,
-    Comment,
-    Private,
-    Hidden,
-}
-
-impl FuncVisibility {
-    pub fn hidden(self) -> bool {
-        self == Self::Hidden
-    }
-
-    pub fn code_visible(self) -> bool {
-        matches!(self, Self::Private | Self::Public)
-    }
-}
-
 #[derive(Clone, Debug)]
 pub struct AsyncTrampoline {
     pub is_method: bool,
@@ -82,7 +64,6 @@ pub struct Info {
     pub glib_name: String,
     pub status: GStatus,
     pub kind: library::FunctionKind,
-    pub func_visibility: FuncVisibility,
     pub visibility: Visibility,
     pub type_name: Result,
     pub parameters: Parameters,
@@ -105,6 +86,11 @@ pub struct Info {
     pub destroys: Vec<Trampoline>,
     pub remove_params: Vec<usize>,
     pub async_future: Option<AsyncFuture>,
+    /// Whether the function is hidden (an implementation detail)
+    /// Like the ref/unref/copy/free functions
+    pub hidden: bool,
+    /// Whether the function can't be generated
+    pub commented: bool,
 }
 
 impl Info {
@@ -123,7 +109,7 @@ impl Info {
     // returns whether the method can be linked in the docs
     pub fn should_be_doc_linked(&self, env: &Env) -> bool {
         self.should_docs_be_generated(env)
-            && (self.status.manual() || self.func_visibility.code_visible())
+            && (self.status.manual() || (!self.commented && !self.hidden))
     }
 
     pub fn should_docs_be_generated(&self, env: &Env) -> bool {
@@ -890,11 +876,6 @@ fn analyze_function(
         bounds.update_imports(imports);
     }
 
-    let func_visibility = if commented {
-        FuncVisibility::Comment
-    } else {
-        FuncVisibility::Public
-    };
     let is_method = func.kind == library::FunctionKind::Method;
     let assertion =
         assertion.unwrap_or_else(|| SafetyAssertionMode::of(env, is_method, &parameters));
@@ -907,7 +888,6 @@ fn analyze_function(
         status,
         kind: func.kind,
         visibility,
-        func_visibility,
         type_name: RustType::try_new(env, type_tid),
         parameters,
         ret,
@@ -929,6 +909,8 @@ fn analyze_function(
         callbacks,
         destroys,
         remove_params: cross_user_data_check.values().cloned().collect::<Vec<_>>(),
+        commented,
+        hidden: false,
     }
 }
 

src/codegen/function.rs
@@ -9,9 +9,7 @@ use super::{
     special_functions,
 };
 use crate::{
-    analysis::{
-        self, bounds::Bounds, functions::FuncVisibility, namespaces, try_from_glib::TryFromGlib,
-    },
+    analysis::{self, bounds::Bounds, namespaces, try_from_glib::TryFromGlib},
     chunk::{ffi_function_todo, Chunk},
     env::Env,
     library,
@@ -51,29 +49,18 @@ pub fn generate(
 
     let mut commented = false;
     let mut comment_prefix = "";
-    let mut pub_prefix = if in_trait {
+    let pub_prefix = if in_trait {
         "".to_owned()
     } else {
         format!("{} ", analysis.visibility)
     };
 
-    match analysis.func_visibility {
-        FuncVisibility::Public => {}
-        FuncVisibility::Comment => {
-            commented = true;
-            comment_prefix = "//";
-        }
-        FuncVisibility::Private => {
-            if in_trait {
-                warn!(
-                    "Generating trait method for private function {}",
-                    analysis.glib_name
-                );
-            } else {
-                pub_prefix = "".to_string();
-            }
-        }
-        FuncVisibility::Hidden => return Ok(()),
+    if analysis.commented {
+        commented = true;
+        comment_prefix = "//";
+    }
+    if analysis.hidden {
+        return Ok(());
     }
     let unsafe_ = if analysis.unsafe_ { "unsafe " } else { "" };
     let declaration = declaration(env, analysis);
@@ -94,7 +81,8 @@ pub fn generate(
             doc_alias(w, &analysis.func_name, comment_prefix, indent)?;
         }
     }
-    let dead_code_cfg = if !analysis.visibility.is_public() {
+    // Don't add a guard for public or copy/equal functions
+    let dead_code_cfg = if !analysis.visibility.is_public() && !analysis.is_special() {
         "#[allow(dead_code)]"
     } else {
         ""
@@ -307,7 +295,7 @@ pub fn bounds(
 }
 
 pub fn body_chunk(env: &Env, analysis: &analysis::functions::Info) -> Chunk {
-    if analysis.func_visibility == FuncVisibility::Comment {
+    if analysis.commented {
         return ffi_function_todo(env, &analysis.glib_name);
     }
 

src/codegen/general.rs
@@ -847,7 +847,7 @@ pub fn declare_default_from_new(
     has_builder: bool,
 ) -> Result<()> {
     if let Some(func) = functions.iter().find(|f| {
-        !f.func_visibility.hidden()
+        !f.hidden
             && f.status.need_generate()
             && f.name == "new"
             // Cannot generate Default implementation for Option<>

src/codegen/record.rs
@@ -90,7 +90,7 @@ pub fn generate(w: &mut dyn Write, env: &Env, analysis: &analysis::record::Info)
     if analysis
         .functions
         .iter()
-        .any(|f| f.status.need_generate() && !f.func_visibility.hidden())
+        .any(|f| f.status.need_generate() && !f.hidden)
     {
         writeln!(w)?;
         write!(w, "impl {} {{", analysis.name)?;

commit e983bc43a89f5c0fd5b6a5981b04734265c85d5f

codegen/special_functions: Add missing space between `pub` and `fn`
Originally the `visibility` modifier contained this space ("pub ") to
keep indentation in check.  A recent change however formats the
visibility modifier from configuration directly into the file, without
inserting the necessary trailing space, resulting in the text `pubfn` in
`special_functions`, which is invalid syntax.

Since output is almost always formatted with rustfmt anyway, this
indentation mismatch is irrelevant to allocate a string for here.

Fixes: e3bd76a3 ("replace FunctionVisibility with hidden/commented attributes")

src/codegen/special_functions.rs
@@ -40,7 +40,7 @@ pub(super) fn generate_static_to_str(
     writeln!(
         w,
         "\
-\t{visibility}fn {rust_fn_name}<'a>(self) -> &'a str {{
+\t{visibility} fn {rust_fn_name}<'a>(self) -> &'a str {{
 \t\tunsafe {{
 \t\t\tCStr::from_ptr(
 \t\t\t\t{ns}::{glib_fn_name}(self.into_glib())