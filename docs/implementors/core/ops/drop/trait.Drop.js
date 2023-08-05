dafbe0b38355400e9a70a3b50b8289dd2f1d16a6
GuillaumeGomez <GuillaumeGomez@users.noreply.github.com>
2023-08-05 10:17:14 +0000

    deploy: 47c2f0765a18bf37dcafe0fbc66af1ae76d78dfd

--- docs/implementors/core/ops/drop/trait.Drop.js
+++ docs/implementors/core/ops/drop/trait.Drop.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"libgir":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"libgir/analysis/imports/struct.ImportsWithDefault.html\" title=\"struct libgir::analysis::imports::ImportsWithDefault\">ImportsWithDefault</a>&lt;'_&gt;"]]
+"libgir":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"libgir/analysis/imports/struct.ImportsWithDefault.html\" title=\"struct libgir::analysis::imports::ImportsWithDefault\">ImportsWithDefault</a>&lt;'_&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
