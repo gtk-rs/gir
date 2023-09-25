ee4f1260d14c7d83c6b74e3a810da1f6a43bb6cb
sdroege <sdroege@users.noreply.github.com>
2023-09-25 06:22:06 +0000

    deploy: 73df75128d555e3d83cebc3c28bde56dd27a7ccd

--- docs/implementors/core/fmt/trait.Display.js
+++ docs/implementors/core/fmt/trait.Display.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"libgir":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"libgir/library/enum.Type.html\" title=\"enum libgir::library::Type\">Type</a>"]]
+"libgir":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"libgir/library/enum.Type.html\" title=\"enum libgir::library::Type\">Type</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
