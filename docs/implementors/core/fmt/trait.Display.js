2dc4720955908f749b62dfda0ad7311b39cbc68b
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2024-01-18 14:22:57 +0000

    deploy: 1c7a6b57a5fcc4d87cb82e0535dc49d14babb76b

--- docs/implementors/core/fmt/trait.Display.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"libgir":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"libgir/library/enum.Type.html\" title=\"enum libgir::library::Type\">Type</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ docs/trait.impl/core/ops/index/trait.Index.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"libgir":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.75.0/std/primitive.u16.html\">u16</a>&gt; for <a class=\"struct\" href=\"libgir/analysis/namespaces/struct.Info.html\" title=\"struct libgir::analysis::namespaces::Info\">Info</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
