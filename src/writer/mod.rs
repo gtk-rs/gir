5cf4689229721e0cd5ad18c04f47b8373da78115
Bilal Elmoussaoui <belmouss@redhat.com>
2024-10-22 12:35:21 +0200

    cleanup: Use Display for SafetyAssertionMode
    Instead of that weird function

--- src/writer/mod.rs
+++ src/writer/mod.rs
@@ -4,13 +4,3 @@ pub mod to_code; // TODO:remove pub
 pub mod untabber;
 
 pub use self::{defines::TAB, to_code::ToCode};
-use crate::analysis::safety_assertion_mode::SafetyAssertionMode;
-
-pub fn safety_assertion_mode_to_str(s: SafetyAssertionMode) -> &'static str {
-    match s {
-        SafetyAssertionMode::None => "",
-        SafetyAssertionMode::NotInitialized => "assert_not_initialized!();",
-        SafetyAssertionMode::Skip => "skip_assert_initialized!();",
-        SafetyAssertionMode::InMainThread => "assert_initialized_main_thread!();",
-    }
-}
