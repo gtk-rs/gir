c08f93955b14a95d1c2a9d24dfc89abd6cb874d9
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2024-10-22 10:47:02 +0000

    deploy: a172dda2a7283d286d45c237b0f8bc39b5354e94

--- docs/trait.impl/core/fmt/trait.Display.js
+++ docs/trait.impl/core/fmt/trait.Display.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["libgir",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"libgir/library/enum.Type.html\" title=\"enum libgir::library::Type\">Type</a>"]]]]);
+    var implementors = Object.fromEntries([["libgir",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"libgir/analysis/safety_assertion_mode/enum.SafetyAssertionMode.html\" title=\"enum libgir::analysis::safety_assertion_mode::SafetyAssertionMode\">SafetyAssertionMode</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"libgir/library/enum.Type.html\" title=\"enum libgir::library::Type\">Type</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[264]}
+//{"start":57,"fragment_lengths":[608]}
