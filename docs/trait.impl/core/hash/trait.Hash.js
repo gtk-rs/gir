1a3a0ee0edbc7ec075536326efa37aef41e1d664
sdroege <sdroege@users.noreply.github.com>
2025-03-19 11:39:32 +0000

    deploy: 87cddb70c739f25edd8047e6780e3934af8ff474

--- docs/trait.impl/core/hash/trait.Hash.js
+++ docs/trait.impl/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["libgir",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"libgir/library/struct.TypeId.html\" title=\"struct libgir::library::TypeId\">TypeId</a>"]]]]);
+    var implementors = Object.fromEntries([["libgir",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"libgir/library/struct.TypeId.html\" title=\"struct libgir::library::TypeId\">TypeId</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
