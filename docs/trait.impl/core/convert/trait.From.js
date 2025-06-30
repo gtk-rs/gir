2679ce439fba0e5aa102a730f9f7f5b74d8cc4ab
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2025-06-30 07:13:15 +0000

    deploy: b3d444a058cfe63f628adf221249a1a332044549

--- docs/trait.impl/core/convert/trait.From.js
+++ docs/trait.impl/core/convert/trait.From.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["libgir",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;T&gt; for <a class=\"struct\" href=\"libgir/analysis/rust_type/struct.RustType.html\" title=\"struct libgir::analysis::rust_type::RustType\">RustType</a>"]]]]);
+    var implementors = Object.fromEntries([["libgir",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;T&gt; for <a class=\"struct\" href=\"libgir/analysis/rust_type/struct.RustType.html\" title=\"struct libgir::analysis::rust_type::RustType\">RustType</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
