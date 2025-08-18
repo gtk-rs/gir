94dcfdddfc9656457812e990c5b123b397156836
sdroege <sdroege@users.noreply.github.com>
2025-08-18 06:55:37 +0000

    deploy: ede6f56bee0362b3ff9b4e60650a7eb34d86735e

--- docs/trait.impl/core/ops/index/trait.Index.js
+++ docs/trait.impl/core/ops/index/trait.Index.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["libgir",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.88.0/std/primitive.u16.html\">u16</a>&gt; for <a class=\"struct\" href=\"libgir/analysis/namespaces/struct.Info.html\" title=\"struct libgir::analysis::namespaces::Info\">Info</a>"]]]]);
+    var implementors = Object.fromEntries([["libgir",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.89.0/std/primitive.u16.html\">u16</a>&gt; for <a class=\"struct\" href=\"libgir/analysis/namespaces/struct.Info.html\" title=\"struct libgir::analysis::namespaces::Info\">Info</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
