de4d480e3a5f08e56d9af0d04135671a161f5427
sdroege <sdroege@users.noreply.github.com>
2025-09-19 12:40:14 +0000

    deploy: be9aa145267cb915b3c79c4ae6d896beecc061ee

--- docs/trait.impl/core/ops/drop/trait.Drop.js
+++ docs/trait.impl/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["libgir",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"libgir/analysis/imports/struct.ImportsWithDefault.html\" title=\"struct libgir::analysis::imports::ImportsWithDefault\">ImportsWithDefault</a>&lt;'_&gt;"]]]]);
+    var implementors = Object.fromEntries([["libgir",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"libgir/analysis/imports/struct.ImportsWithDefault.html\" title=\"struct libgir::analysis::imports::ImportsWithDefault\">ImportsWithDefault</a>&lt;'_&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
