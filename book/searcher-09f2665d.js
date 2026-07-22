b9a8f114e33d49dfc09838fd0a57a33ba4ed5286
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2026-07-22 20:11:28 +0000

    deploy: b4ad84f7c5548fbab3972436e1a000fb5f829cdc

--- book/searcher-09f2665d.js
+++ book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-16e7fed0.js',
+                path_to_root + 'searchindex-e346ce47.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
